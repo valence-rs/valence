f5f8264a70cf3be4e24a7c1f365f0e0a457f58e7
Ryan <ryanj00a@gmail.com>
2022-04-19 23:41:15 -0700

    Add Debug impl to BlockState

--- src/block.rs
+++ src/block.rs
@@ -1,6 +1,36 @@
 #![allow(clippy::all)]
 
+use std::fmt;
+
 include!(concat!(env!("OUT_DIR"), "/block.rs"));
+
+impl fmt::Debug for BlockState {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let typ = self.to_type();
+
+        write!(f, "{}", typ.to_str())?;
+
+        let props = typ.props();
+
+        if !props.is_empty() {
+            let mut list = f.debug_list();
+            for &p in typ.props() {
+                struct KeyVal<'a>(&'a str, &'a str);
+
+                impl<'a> fmt::Debug for KeyVal<'a> {
+                    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+                        write!(f, "{}={}", self.0, self.1)
+                    }
+                }
+
+                list.entry(&KeyVal(p.to_str(), self.get(p).unwrap().to_str()));
+            }
+            list.finish()
+        } else {
+            Ok(())
+        }
+    }
+}
 
 #[cfg(test)]
 mod tests {
