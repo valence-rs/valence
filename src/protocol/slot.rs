0652fa13a42db0cde84721a6ca89a85437624b6a
Carson McManus <dyc3@users.noreply.github.com>
2022-10-09 15:38:00 -0400

    change `Slot` enum into alias of `Option<ItemStack>` (#107)

--- src/protocol/slot.rs
+++ src/protocol/slot.rs
@@ -9,18 +9,13 @@ use crate::protocol::{Decode, Encode};
 pub type SlotId = i16;
 
 /// Represents a slot in an inventory.
-#[derive(Clone, Default, Debug)]
-pub enum Slot {
-    #[default]
-    Empty,
-    Present(ItemStack),
-}
+pub type Slot = Option<ItemStack>;
 
 impl Encode for Slot {
     fn encode(&self, w: &mut impl Write) -> anyhow::Result<()> {
         match self {
-            Slot::Empty => false.encode(w),
-            Slot::Present(s) => {
+            None => false.encode(w),
+            Some(s) => {
                 true.encode(w)?;
                 s.item.encode(w)?;
                 s.item_count.encode(w)?;
@@ -37,9 +32,9 @@ impl Decode for Slot {
     fn decode(r: &mut &[u8]) -> anyhow::Result<Self> {
         let present = bool::decode(r)?;
         if !present {
-            return Ok(Slot::Empty);
+            return Ok(None);
         }
-        Ok(Slot::Present(ItemStack {
+        Ok(Some(ItemStack {
             item: ItemKind::decode(r)?,
             item_count: u8::decode(r)?,
             nbt: if r.first() == Some(&0) {
@@ -49,25 +44,5 @@ impl Decode for Slot {
                 Some(Compound::decode(r)?)
             },
         }))
-    }
-}
-
-impl From<Option<ItemStack>> for Slot {
-    fn from(s: Option<ItemStack>) -> Self {
-        if let Some(s) = s {
-            Slot::Present(s)
-        } else {
-            Slot::Empty
-        }
-    }
-}
-
-impl From<Slot> for Option<ItemStack> {
-    fn from(s: Slot) -> Self {
-        if let Slot::Present(s) = s {
-            Some(s)
-        } else {
-            None
-        }
     }
 }
