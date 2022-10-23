e2442b6690dde1b93b8628741e6ab74647ee4e8c
TerminatorNL <TerminatorNL@users.noreply.github.com>
2022-10-24 00:15:47 +0200

    Rust part: Biome parsing

--- src/biomes.rs
+++ src/biomes.rs
@@ -1,4 +1,5 @@
 // biome.rs exposes constant values provided by the build script.
 // All biome variants are located in `BiomeKind`. You can use the
-// associated const fn functions of `BiomeKind` to access details about a biome type.
+// associated const fn functions of `BiomeKind` to access details about a biome
+// type.
 include!(concat!(env!("OUT_DIR"), "/biome.rs"));
