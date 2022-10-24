9e937adb583b5e1e09d659de0a6b3e4fd22b98a0
Ryan <ryanj00a@gmail.com>
2022-10-23 18:32:47 -0700

    Adjustments

--- src/biomes.rs
+++
@@ -1,5 +0,0 @@
-// biome.rs exposes constant values provided by the build script.
-// All biome variants are located in `BiomeKind`. You can use the
-// associated const fn functions of `BiomeKind` to access details about a biome
-// type.
-include!(concat!(env!("OUT_DIR"), "/biome.rs"));
