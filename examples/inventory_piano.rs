b687d318040317f4736254e2511a674257f7d862
Ryan <ryanj00a@gmail.com>
2022-12-11 01:23:30 -0800

    Fix merge

--- examples/inventory_piano.rs
+++ examples/inventory_piano.rs
@@ -164,16 +164,16 @@ impl Config for Game {
                 );
             }
 
+            let player = server.entities.get_mut(client.state.entity_id).unwrap();
+
             if client.is_disconnected() {
                 self.player_count.fetch_sub(1, Ordering::SeqCst);
-                server.entities.remove(client.state.entity_id);
+                player.set_deleted(true);
                 if let Some(id) = &server.state.player_list {
                     server.player_lists.get_mut(id).remove(client.uuid());
                 }
                 return false;
             }
-
-            let player = server.entities.get_mut(client.state.entity_id).unwrap();
 
             if client.position().y <= -20.0 {
                 client.teleport(spawn_pos, client.yaw(), client.pitch());
