671dd895ef0bd5ed15a1ce53f1a52f70192bed88
Ryan <ryanj00a@gmail.com>
2022-12-20 21:54:01 -0800

    Add packet encode/decode benchmarks

--- valence_protocol/benches/benches.rs
+++ valence_protocol/benches/benches.rs
@@ -1,14 +1,22 @@
 use std::time::Duration;
 
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
-use valence_protocol::block::{PropName, PropValue};
-use valence_protocol::{BlockKind, BlockState, ItemKind};
+use valence_nbt::{compound, List};
+use valence_protocol::block::{BlockKind, BlockState, PropName, PropValue};
+use valence_protocol::packets::s2c::play::{
+    ChunkDataAndUpdateLight, ChunkDataAndUpdateLightEncode, SetTabListHeaderAndFooter,
+};
+use valence_protocol::text::Color;
+use valence_protocol::{
+    write_packet, write_packet_compressed, ItemKind, LengthPrefixedArray, PacketDecoder,
+    PacketEncoder, TextFormat,
+};
 
 criterion_group! {
     name = benches;
     config = Criterion::default()
-        .measurement_time(Duration::from_secs(3));
-    targets = blocks
+        .measurement_time(Duration::from_secs(5)).confidence_level(0.99);
+    targets = blocks, packets
 }
 criterion_main!(benches);
 
@@ -92,6 +100,166 @@ fn blocks(c: &mut Criterion) {
             for kind in black_box(ItemKind::ALL) {
                 black_box(BlockKind::from_item_kind(kind));
             }
+        });
+    });
+}
+
+fn packets(c: &mut Criterion) {
+    let mut encoder = PacketEncoder::new();
+
+    const BLOCKS_AND_BIOMES: [u8; 2000] = [0x80; 2000];
+    const SKY_LIGHT_ARRAYS: [LengthPrefixedArray<u8, 2048>; 26] =
+        [LengthPrefixedArray([0xff; 2048]); 26];
+
+    let chunk_data_packet = ChunkDataAndUpdateLightEncode {
+        chunk_x: 123,
+        chunk_z: 456,
+        heightmaps: &compound! {
+            "MOTION_BLOCKING" => List::Long(vec![123; 256]),
+        },
+        blocks_and_biomes: BLOCKS_AND_BIOMES.as_slice(),
+        block_entities: &[],
+        trust_edges: false,
+        sky_light_mask: &[],
+        block_light_mask: &[],
+        empty_sky_light_mask: &[],
+        empty_block_light_mask: &[],
+        sky_light_arrays: SKY_LIGHT_ARRAYS.as_slice(),
+        block_light_arrays: &[],
+    };
+
+    let tab_list_header_footer_packet = SetTabListHeaderAndFooter {
+        header: "this".italic() + " is the " + "header".bold().color(Color::RED),
+        footer: "this".italic()
+            + " is the "
+            + "footer".bold().color(Color::BLUE)
+            + ". I am appending some extra text so that the packet goes over the compression \
+               threshold.",
+    };
+
+    c.bench_function("encode_chunk_data", |b| {
+        b.iter(|| {
+            let encoder = black_box(&mut encoder);
+
+            encoder.clear();
+            encoder.append_packet(&chunk_data_packet).unwrap();
+
+            black_box(encoder);
+        });
+    });
+
+    c.bench_function("encode_tab_list_header_footer", |b| {
+        b.iter(|| {
+            let encoder = black_box(&mut encoder);
+
+            encoder.clear();
+            encoder
+                .append_packet(&tab_list_header_footer_packet)
+                .unwrap();
+
+            black_box(encoder);
+        });
+    });
+
+    encoder.set_compression(Some(256));
+
+    c.bench_function("encode_chunk_data_compressed", |b| {
+        b.iter(|| {
+            let encoder = black_box(&mut encoder);
+
+            encoder.clear();
+            encoder.append_packet(&chunk_data_packet).unwrap();
+
+            black_box(encoder);
+        });
+    });
+
+    c.bench_function("encode_tab_list_header_footer_compressed", |b| {
+        b.iter(|| {
+            let encoder = black_box(&mut encoder);
+
+            encoder.clear();
+            encoder
+                .append_packet(&tab_list_header_footer_packet)
+                .unwrap();
+
+            black_box(encoder);
+        });
+    });
+
+    let mut chunk_data = vec![];
+    let mut tab_list_header_footer = vec![];
+
+    write_packet(&mut chunk_data, &chunk_data_packet).unwrap();
+    write_packet(&mut tab_list_header_footer, &tab_list_header_footer_packet).unwrap();
+
+    let mut decoder = PacketDecoder::new();
+
+    c.bench_function("decode_chunk_data", |b| {
+        b.iter(|| {
+            let decoder = black_box(&mut decoder);
+
+            decoder.queue_slice(&chunk_data);
+            decoder
+                .try_next_packet::<ChunkDataAndUpdateLight>()
+                .unwrap();
+
+            black_box(decoder);
+        });
+    });
+
+    c.bench_function("decode_tab_list_header_footer", |b| {
+        b.iter(|| {
+            let decoder = black_box(&mut decoder);
+
+            decoder.queue_slice(&tab_list_header_footer);
+            decoder
+                .try_next_packet::<SetTabListHeaderAndFooter>()
+                .unwrap();
+
+            black_box(decoder);
+        });
+    });
+
+    decoder.set_compression(true);
+
+    let mut scratch = vec![];
+
+    chunk_data.clear();
+    write_packet_compressed(&mut chunk_data, 256, &mut scratch, &chunk_data_packet).unwrap();
+
+    tab_list_header_footer.clear();
+    write_packet_compressed(
+        &mut tab_list_header_footer,
+        256,
+        &mut scratch,
+        &tab_list_header_footer_packet,
+    )
+    .unwrap();
+
+    c.bench_function("decode_chunk_data_compressed", |b| {
+        b.iter(|| {
+            let decoder = black_box(&mut decoder);
+
+            decoder.queue_slice(&chunk_data);
+            decoder
+                .try_next_packet::<ChunkDataAndUpdateLight>()
+                .unwrap();
+
+            black_box(decoder);
+        });
+    });
+
+    c.bench_function("decode_tab_list_header_footer_compressed", |b| {
+        b.iter(|| {
+            let decoder = black_box(&mut decoder);
+
+            decoder.queue_slice(&tab_list_header_footer);
+            decoder
+                .try_next_packet::<SetTabListHeaderAndFooter>()
+                .unwrap();
+
+            black_box(decoder);
         });
     });
 }
