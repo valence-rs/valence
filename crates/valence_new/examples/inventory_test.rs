23dc9bfc0801126329ee2d55f3b6a71239d20a1c
Carson McManus <carson.mcmanus1@gmail.com>
2023-01-14 12:05:34 -0500

    add inventory_test example

---
+++ crates/valence_new/examples/inventory_test.rs
@@ -0,0 +1,85 @@
+use bevy_ecs::prelude::*;
+use bevy_ecs::schedule::ShouldRun;
+use tracing::info;
+use valence_new::client::Client;
+use valence_new::config::Config;
+use valence_new::dimension::DimensionId;
+use valence_new::inventory::{Inventory, OpenInventory};
+use valence_new::protocol::types::GameMode;
+use valence_new::server::Server;
+use valence_protocol::{InventoryKind, ItemKind, ItemStack};
+
+#[derive(Resource)]
+struct GameState {
+    instance: Entity,
+    inventory: usize,
+}
+
+fn main() -> anyhow::Result<()> {
+    tracing_subscriber::fmt().init();
+
+    valence_new::run_server(
+        Config::default(),
+        SystemStage::parallel()
+            .with_system(setup.with_run_criteria(ShouldRun::once))
+            .with_system(init_clients)
+            .with_system(open_inventory_test),
+        (),
+    )
+}
+
+fn setup(world: &mut World) {
+    let instance = world
+        .resource::<Server>()
+        .new_instance(DimensionId::default());
+
+    let id = world.spawn(instance).id();
+    world.insert_resource(GameState {
+        instance: id,
+        inventory: 0,
+    });
+
+    // create inventories to view
+    let mut inventories = [
+        Inventory::new(InventoryKind::Generic9x2),
+        Inventory::new(InventoryKind::Generic9x3),
+        Inventory::new(InventoryKind::Crafting),
+    ];
+
+    for inv in inventories.iter_mut() {
+        inv.replace_slot(0, Some(ItemStack::new(ItemKind::DiamondPickaxe, 1, None)));
+    }
+
+    for inv in inventories {
+        world.spawn(inv);
+    }
+}
+
+fn init_clients(mut clients: Query<&mut Client, Added<Client>>, state: Res<GameState>) {
+    for mut client in &mut clients {
+        client.set_instance(state.instance);
+        client.set_game_mode(GameMode::Creative);
+    }
+}
+
+fn open_inventory_test(
+    mut state: ResMut<GameState>,
+    mut commands: Commands,
+    clients: Query<(Entity, With<Client>, Without<OpenInventory>)>,
+    inventories: Query<(Entity, With<Inventory>, Without<Client>)>,
+) {
+    if clients.is_empty() {
+        return;
+    }
+    if state.inventory >= inventories.iter().count() {
+        state.inventory = 0;
+    }
+    let (target_inventory, _, _) = inventories.iter().skip(state.inventory).next().unwrap();
+    info!("opening inventory {}", state.inventory);
+    for (entity, _, _) in &clients {
+        commands
+            .entity(entity)
+            .insert(OpenInventory::new(target_inventory));
+    }
+    state.inventory += 1;
+}
