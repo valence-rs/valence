160609045125054e1f25ffab67a553d315b4c496
Carson McManus <carson.mcmanus1@gmail.com>
2023-01-20 12:19:12 -0500

    fix order of state id increment and aliasing

--- crates/valence_new/src/inventory.rs
+++ crates/valence_new/src/inventory.rs
@@ -134,9 +134,9 @@ pub(crate) fn update_player_inventories(
                 client.cursor_item_modified = false;
             } else {
                 // send the modified slots
-                let state_id = client.inventory_state_id.0;
                 if inventory.modified & !client.inventory_slots_modified != 0 {
                     client.inventory_state_id += 1;
+                    let state_id = client.inventory_state_id.0;
                     for (i, slot) in inventory.slots.iter().enumerate() {
                         if (inventory.modified >> i) & 1 == 1 {
                             if (client.inventory_slots_modified >> i) & 1 == 1 {
@@ -386,9 +386,9 @@ pub(crate) fn update_open_inventories(
             } else {
                 // send the modified slots
                 let window_id = client.window_id as i8;
-                let state_id = client.inventory_state_id.0;
                 if inventory.modified & !open_inventory.client_modified != 0 {
                     client.inventory_state_id += 1;
+                    let state_id = client.inventory_state_id.0;
                     for (i, slot) in inventory.slots.iter().enumerate() {
                         if (inventory.modified >> i) & 1 == 1 {
                             if (open_inventory.client_modified >> i) & 1 == 1 {
