199ab18b808342e98e052b8babff6801838ed0e0
Carson McManus <carson.mcmanus1@gmail.com>
2023-02-02 09:43:01 -0500

    reimpl MockClientHelper so it uses MockClientConnection

--- crates/valence_new/src/unit_test/util.rs
+++ crates/valence_new/src/unit_test/util.rs
@@ -1,5 +1,4 @@
-use std::sync::{Arc, Mutex};
-
+use valence_protocol::packets::S2cPlayPacket;
 use valence_protocol::{EncodePacket, PacketDecoder, PacketEncoder, Username};
 
 use crate::client::Client;
@@ -14,8 +13,8 @@ pub fn create_mock_client(client_info: NewClientInfo) -> (Client, MockClientHelp
     let mock_connection = MockClientConnection::new();
     let enc = PacketEncoder::new();
     let dec = PacketDecoder::new();
-    let client = Client::new(client_info, Box::new(mock_connection), enc, dec);
-    (client, MockClientHelper {})
+    let client = Client::new(client_info, Box::new(mock_connection.clone()), enc, dec);
+    (client, MockClientHelper::new(mock_connection))
 }
 
 /// Creates a `NewClientInfo` with the given username and a random UUID.
@@ -29,6 +28,40 @@ pub fn gen_client_info(username: &str) -> NewClientInfo {
     }
 }
 
+/// Contains the mocked client connection and helper methods to inject packets
+/// and read packets from the send stream.
 pub struct MockClientHelper {
-    // stream: Arc<Mutex<MockPacketStream>>,
+    conn: MockClientConnection,
+    enc: PacketEncoder,
+    dec: PacketDecoder,
+}
+
+impl MockClientHelper {
+    fn new(conn: MockClientConnection) -> Self {
+        Self {
+            conn,
+            enc: PacketEncoder::new(),
+            dec: PacketDecoder::new(),
+        }
+    }
+
+    /// Inject a packet to be treated as a packet inbound to the server. Panics
+    /// if the packet cannot be sent.
+    pub fn send(&mut self, packet: &impl EncodePacket) {
+        self.enc
+            .append_packet(packet)
+            .expect("failed to encode packet");
+        self.conn.inject_recv(self.enc.take());
+    }
+
+    /// Collect all packets that have been sent to the client.
+    pub fn collect_sent<'a>(&'a mut self) -> anyhow::Result<Vec<S2cPlayPacket<'a>>> {
+        self.dec.queue_bytes(self.conn.take_sent());
+
+        self.dec.collect_into_vec::<S2cPlayPacket<'a>>()
+    }
+
+    pub fn clear_sent(&mut self) {
+        self.conn.clear_sent();
+    }
 }
