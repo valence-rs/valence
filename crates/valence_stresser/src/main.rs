4fcea01f13214eb1cad6972993744c65563e4a97
qualterz <qualterz@tutamail.com>
2023-02-14 16:03:17 +0200

    Define minimal CLI options

--- crates/valence_stresser/src/main.rs
+++ crates/valence_stresser/src/main.rs
@@ -1,2 +1,13 @@
+struct Args {
+    /// Represents an IPv4/IPv6/DNS address of a server.
+    target_host: String,
+
+    /// Represents a port of the target host.
+    target_port: u16,
+
+    /// Represents an amount of connections to the target host.
+    connections: usize
+}
+
 fn main() {
 }
