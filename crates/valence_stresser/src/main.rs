bf68fc9941e0971d1595ea8131baa68142fb6005
qualterz <qualterz@tutamail.com>
2023-02-14 16:44:57 +0200

    Fill out the cli args struct

--- crates/valence_stresser/src/main.rs
+++ crates/valence_stresser/src/main.rs
@@ -1,13 +1,21 @@
+use clap::{arg, command, Parser};
+
+#[derive(Parser)]
+#[command(author, version, about)]
 struct StresserArgs {
     /// Represents an IPv4/IPv6/DNS address of a server.
+    #[arg(short = 't', long = "host")]
     target_host: String,
 
     /// Represents a port of the target host.
+    #[arg(short = 'p', long = "port")]
     target_port: u16,
 
     /// Represents an amount of connections to the target host.
-    connections_count: usize
+    #[arg(short = 'c', long = "count")]
+    connections_count: usize,
 }
 
 fn main() {
+    let args = StresserArgs::parse();
 }
