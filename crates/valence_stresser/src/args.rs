39f92b63d0b9ac0bb6f746e5328bc9c12fa0c0af
qualterz <qualterz@tutamail.com>
2023-02-16 19:06:10 +0200

    Remove port option

--- crates/valence_stresser/src/args.rs
+++ crates/valence_stresser/src/args.rs
@@ -7,11 +7,6 @@ pub(crate) struct StresserArgs {
     #[arg(short = 't', long = "target")]
     pub target_host: String,
 
-    /// Port of the target host.
-    #[arg(short = 'p', long = "port")]
-    #[arg(default_value_t = 25565)]
-    pub target_port: u16,
-
     /// Number of connections to the target.
     #[arg(short = 'c', long = "count")]
     pub connections_count: usize,
