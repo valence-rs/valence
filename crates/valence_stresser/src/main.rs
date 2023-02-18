4802514be246b6b23af826c763af5ea8057c2382
qualterz <qualterz@tutamail.com>
2023-02-18 18:34:36 +0200

    Implement multithreaded connections

--- crates/valence_stresser/src/main.rs
+++ crates/valence_stresser/src/main.rs
@@ -1,4 +1,6 @@
+use core::time::Duration;
 use std::net::ToSocketAddrs;
+use std::thread::{self, JoinHandle};
 
 use args::StresserArgs;
 use clap::Parser;
@@ -12,5 +14,19 @@ fn main() {
 
     let target_addr = args.target_host.to_socket_addrs().unwrap().next().unwrap();
 
-    make_connection(target_addr, args.name_prefix.as_str());
+    let mut last_thread: Option<JoinHandle<()>> = None;
+
+    for conn_index in 0..args.connections_count {
+        let conn_name = format!("{}{}", args.name_prefix, conn_index);
+
+        last_thread = Some(thread::spawn(move || {
+            make_connection(target_addr, &conn_name.as_str());
+        }));
+
+        thread::sleep(Duration::from_millis(args.spawn_cooldown))
+    }
+
+    if let Some(thread) = last_thread {
+        _ = thread.join();
+    }
 }
