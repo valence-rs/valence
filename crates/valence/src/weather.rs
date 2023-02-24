4fb5c0add446ef29ab28a18500fb77ef7bdd1559
qualterz <qualterz@tutamail.com>
2023-02-25 01:17:34 +0200

    Add weather level constants

--- crates/valence/src/weather.rs
+++ crates/valence/src/weather.rs
@@ -1,10 +1,19 @@
+use bevy_ecs::prelude::*;
+
+pub const MIN_WEATHER_LEVEL: f32 = 0_f32;
+pub const MAX_WEATHER_LEVEL: f32 = 1_f32;
+
 /// The weather state representation.
 #[derive(Component)]
 pub struct Weather {
     /// Contains the raining level.
-    /// The `None` value means there is no raining event.
+    /// Should be between [`MIN_WEATHER_LEVEL`] and [`MAX_WEATHER_LEVEL`].
+    ///
+    /// The [`None`] value means no raining event.
     pub raining: Option<f32>,
     /// Contains the thunder level.
-    /// The `None` value means there is no thunder.
-    pub thunder: Option<f32>
+    /// Should be between [`MIN_WEATHER_LEVEL`] and [`MAX_WEATHER_LEVEL`].
+    ///
+    /// The [`None`] value means no thunder.
+    pub thunder: Option<f32>,
 }
