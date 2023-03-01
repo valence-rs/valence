b7d671aa896d7a9e89a80e954e1597e7ad312439
MrlnHi <marlonclaus2004@gmail.com>
2023-03-01 11:59:15 +0100

    Implement schem saving

--- crates/valence_schem/src/lib.rs
+++ crates/valence_schem/src/lib.rs
@@ -20,7 +20,7 @@
 use std::collections::hash_map::Entry;
 use std::collections::HashMap;
 use std::fs::File;
-use std::io::{self, BufReader, Read};
+use std::io::{self, BufReader, Read, Write};
 use std::path::PathBuf;
 use std::str::FromStr;
 
@@ -33,8 +33,9 @@ use valence_core::block_pos::BlockPos;
 use valence_core::chunk_pos::ChunkPos;
 use valence_core::ident::Ident;
 use valence_core::packet::var_int::{VarInt, VarIntDecodeError};
+use valence_core::packet::Encode;
 use valence_instance::{Block as ValenceBlock, Instance};
-use valence_nbt::{Compound, List, Value};
+use valence_nbt::{compound, Compound, List, Value};
 
 #[derive(Debug, Clone, PartialEq)]
 pub struct Schematic {
@@ -187,6 +188,18 @@ impl<I: ExactSizeIterator<Item = u8>> Iterator for VarIntReader<I> {
         } else {
             Some(VarInt::decode_partial(ReadWrapper(&mut self.0)))
         }
+    }
+}
+
+struct VarIntWriteWrapper<'a>(&'a mut Vec<i8>);
+impl<'a> Write for VarIntWriteWrapper<'a> {
+    fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
+        self.0.extend(buf.iter().map(|byte| *byte as i8));
+        Ok(buf.len())
+    }
+
+    fn flush(&mut self) -> io::Result<()> {
+        Ok(())
     }
 }
 
@@ -409,6 +422,125 @@ impl Schematic {
         })
     }
 
+    pub fn serialize(&self) -> Compound {
+        let mut compound = compound! {
+            "Version" => 3,
+            "DataVersion" => 3218,
+            "Width" => self.width as i16,
+            "Height" => self.height as i16,
+            "Length" => self.length as i16,
+        };
+        if let Some(metadata) = &self.metadata {
+            compound.insert("Metadata", metadata.clone());
+        }
+        match self.offset {
+            IVec3::ZERO => {}
+            IVec3 { x, y, z } => {
+                compound.insert("Offset", vec![x, y, z]);
+            }
+        }
+        if let Some(blocks) = &self.blocks {
+            let blocks: Compound = {
+                let mut palette = HashMap::new();
+                let mut data: Vec<i8> = vec![];
+                let mut block_entities = vec![];
+                for (idx, block) in blocks.iter().enumerate() {
+                    let palette_len = palette.len();
+                    let i = *palette.entry(block.state).or_insert(palette_len);
+                    struct WriteWrapper<'a>(&'a mut Vec<i8>);
+                    impl<'a> Write for WriteWrapper<'a> {
+                        fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
+                            self.0.extend(buf.iter().map(|byte| *byte as i8));
+                            Ok(buf.len())
+                        }
+
+                        fn flush(&mut self) -> io::Result<()> {
+                            Ok(())
+                        }
+                    }
+
+                    VarInt(i as i32).encode(WriteWrapper(&mut data)).unwrap();
+                    if let Some(BlockEntity { kind, data }) = &block.block_entity {
+                        let idx = u16::try_from(idx).unwrap();
+                        let y = idx / (self.width * self.length);
+                        let z = (idx % (self.width * self.length)) / self.width;
+                        let x = (idx % (self.width * self.length)) % self.width;
+
+                        let mut block_entity = compound! {
+                            "Pos" => vec![x as i32, y as i32, z as i32],
+                            "Id" => kind.ident(),
+                        };
+                        if !data.is_empty() {
+                            block_entity.insert("Data", data.clone());
+                        }
+                        block_entities.push(block_entity);
+                    }
+                }
+
+                let palette: Compound = palette
+                    .into_iter()
+                    .map(|(state, idx)| (state.to_string(), Value::Int(idx as i32)))
+                    .collect();
+                let mut blocks = compound! {
+                    "Palette" => palette,
+                    "Data" => data,
+                };
+                if !block_entities.is_empty() {
+                    blocks.insert("BlockEntities", Value::List(List::Compound(block_entities)));
+                }
+                blocks
+            };
+            compound.insert("Blocks", blocks);
+        }
+
+        if let Some(biomes) = &self.biomes {
+            let palette: Compound = biomes
+                .palette
+                .iter()
+                .enumerate()
+                .map(|(idx, val)| (val.to_string(), Value::Int(idx as i32)))
+                .collect();
+            let mut data = vec![];
+            for i in biomes.data.iter() {
+                VarInt(*i as i32)
+                    .encode(VarIntWriteWrapper(&mut data))
+                    .unwrap();
+            }
+            compound.insert(
+                "Biomes",
+                compound! {
+                    "Palette" => palette,
+                    "Data" => data,
+                },
+            );
+        }
+
+        if let Some(entities) = &self.entities {
+            let entities = entities
+                .iter()
+                .map(
+                    |Entity {
+                         pos: DVec3 { x, y, z },
+                         id,
+                         data,
+                     }| {
+                        let mut compound = compound! {
+                            "Pos" => Value::List(List::Double(vec![*x, *y, *z])),
+                            "Id" => id.clone(),
+                        };
+                        if let Some(data) = data {
+                            compound.insert("Data", data.clone());
+                        }
+                        compound
+                    },
+                )
+                .collect();
+            compound.insert("Entities", Value::List(List::Compound(entities)));
+        }
+
+        compound
+    }
+
     pub fn paste<F>(&self, instance: &mut Instance, origin: BlockPos, map_biome: F)
     where
         F: FnMut(Ident<&str>) -> BiomeId,
@@ -489,5 +621,222 @@ impl Schematic {
         }
 
         // TODO: Spawn entities
+    }
+
+    pub fn copy<F>(
+        instance: &Instance,
+        corners: (BlockPos, BlockPos),
+        origin: BlockPos,
+        mut map_biome: F,
+    ) -> Self
+    where
+        F: FnMut(BiomeId) -> Ident<String>,
+    {
+        let min = BlockPos::new(
+            corners.0.x.min(corners.1.x),
+            corners.0.y.min(corners.1.y),
+            corners.0.z.min(corners.1.z),
+        );
+        let max = BlockPos::new(
+            corners.0.x.max(corners.1.x),
+            corners.0.y.max(corners.1.y),
+            corners.0.z.max(corners.1.z),
+        );
+        let width = u16::try_from(max.x - min.x + 1).expect("width too large");
+        let height = u16::try_from(max.y - min.y + 1).expect("height too large");
+        let length = u16::try_from(max.z - min.z + 1).expect("length too large");
+        let offset = IVec3::new(min.x - origin.x, min.y - origin.y, min.z - origin.z);
+        let blocks: Vec<_> = (min.y..=max.y)
+            .flat_map(|y| {
+                (min.z..=max.z).flat_map(move |z| {
+                    (min.x..=max.x).map(move |x| {
+                        let Some(block) = instance.block([x, y, z]) else {
+                            todo!("asd");
+                        };
+                        let state = block.state();
+                        let block_entity = block
+                            .nbt()
+                            .and_then(|data| Some((state.block_entity_kind()?, data.clone())))
+                            .map(|(kind, data)| BlockEntity { kind, data });
+                        Block {
+                            state,
+                            block_entity,
+                        }
+                    })
+                })
+            })
+            .collect();
+        let biomes = {
+            let mut palette = vec![];
+            let mut map = HashMap::new();
+            let data: Vec<_> = (min.x..=max.x)
+                .flat_map(|x| {
+                    (min.z..=max.z).flat_map(move |z| {
+                        (min.y..=max.y).map(move |y| {
+                            instance
+                                .chunk(ChunkPos::from_block_pos(BlockPos::new(x, y, z)))
+                                .unwrap()
+                                .biome(
+                                    x.rem_euclid(16) as usize / 4,
+                                    (y - instance.min_y()) as usize / 4,
+                                    z.rem_euclid(16) as usize / 4,
+                                )
+                        })
+                    })
+                })
+                .map(|biome| match map.entry(biome) {
+                    Entry::Occupied(entry) => *entry.get(),
+                    Entry::Vacant(entry) => {
+                        let idx = palette.len();
+                        palette.push(map_biome(biome));
+                        entry.insert(idx);
+                        idx
+                    }
+                })
+                .collect();
+
+            Biomes {
+                palette: palette.into_boxed_slice(),
+                data: data.into_boxed_slice(),
+            }
+        };
+        Self {
+            metadata: None,
+            width,
+            height,
+            length,
+            offset,
+            blocks: Some(blocks.into_boxed_slice()),
+            biomes: Some(biomes),
+            entities: None, // TODO
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn schematic() {
+        let mut app = App::new();
+        app.add_plugin(ServerPlugin::new(()));
+        let mut instance = app
+            .world
+            .resource::<Server>()
+            .new_instance(DimensionId::default());
+
+        for x in -1..=0 {
+            for z in -1..=0 {
+                instance.insert_chunk([x, z], Chunk::default());
+            }
+        }
+
+        instance.set_block([5, 1, -1], BlockState::GLOWSTONE);
+        instance.set_block([5, 2, -1], BlockState::STONE);
+        instance.set_block([5, 2, -2], BlockState::GLOWSTONE);
+        instance.set_block([4, 2, -1], BlockState::LAPIS_BLOCK);
+        instance.set_block([6, 2, -1], BlockState::STONE);
+        instance.set_block(
+            [5, 3, -1],
+            Block::with_nbt(
+                BlockState::OAK_SIGN,
+                compound! {"Text1" => "abc".into_text()},
+            ),
+        );
+        instance.set_block(
+            [5, 2, 0],
+            BlockState::ANDESITE_WALL
+                .set(PropName::Up, PropValue::True)
+                .set(PropName::North, PropValue::Low),
+        );
+
+        let schematic = Schematic::copy(
+            &instance,
+            (BlockPos::new(4, 3, -1), BlockPos::new(6, 1, 0)),
+            BlockPos::new(5, 3, 0),
+            |_| ident!("minecraft:plains"),
+        );
+
+        schematic.paste(&mut instance, BlockPos::new(15, 18, 16), |_| {
+            BiomeId::default()
+        });
+
+        let block = instance.block([15, 18, 15]).unwrap();
+        assert_eq!(block.state(), BlockState::OAK_SIGN);
+        assert_eq!(block.nbt(), Some(&compound! {"Text1" => "abc".into_text()}));
+
+        let block = instance.block([15, 17, 16]).unwrap();
+        assert_eq!(
+            block.state(),
+            BlockState::ANDESITE_WALL
+                .set(PropName::Up, PropValue::True)
+                .set(PropName::North, PropValue::Low)
+        );
+        assert_eq!(block.nbt(), None);
+
+        let block = instance.block([15, 17, 15]).unwrap();
+        assert_eq!(block.state(), BlockState::STONE);
+        assert_eq!(block.nbt(), None);
+
+        let block = instance.block([15, 17, 14]).unwrap();
+        assert_eq!(block.state(), BlockState::AIR);
+        assert_eq!(block.nbt(), None);
+
+        let block = instance.block([14, 17, 15]).unwrap();
+        assert_eq!(block.state(), BlockState::LAPIS_BLOCK);
+        assert_eq!(block.nbt(), None);
+
+        let block = instance.block([16, 17, 15]).unwrap();
+        assert_eq!(block.state(), BlockState::STONE);
+        assert_eq!(block.nbt(), None);
+
+        let block = instance.block([15, 16, 15]).unwrap();
+        assert_eq!(block.state(), BlockState::GLOWSTONE);
+        assert_eq!(block.nbt(), None);
+
+        let mut schematic = schematic;
+        schematic.metadata.replace(compound! {"A" => 123});
+        let nbt = schematic.serialize();
+        assert_eq!(
+            nbt,
+            compound! {
+                "Version" => 3,
+                "DataVersion" => 3218,
+                "Metadata" => compound! {
+                    "A" => 123,
+                },
+                "Width" => 3i16,
+                "Height" => 3i16,
+                "Length" => 2i16,
+                "Offset" => vec![-1, -2, -1],
+                "Blocks" => compound! {
+                    "Data" => vec![0i8, 1, 0, 0, 0, 0, 2, 3, 3, 0, 4, 0, 0, 5, 0, 0, 0, 0],
+                    "Palette" => compound! {
+                        "air" => 0,
+                        "glowstone" => 1,
+                        "lapis_block" => 2,
+                        "stone" => 3,
+                        "andesite_wall[east=none, north=low, south=none, up=true, waterlogged=false, west=none]" => 4,
+                        "oak_sign[rotation=0, waterlogged=false]" => 5,
+                    },
+                    "BlockEntities" => Value::List(List::Compound(vec![
+                        compound! {
+                            "Data" => compound!{
+                                "Text1" => "abc".into_text(),
+                            },
+                            "Id" => "sign",
+                            "Pos" => vec![1, 2, 0],
+                        },
+                    ]))
+                },
+                "Biomes" => compound! {
+                    "Data" => vec![0i8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                    "Palette" => compound! {
+                        "minecraft:plains" => 0,
+                    }
+                },
+            }
+        );
     }
 }
