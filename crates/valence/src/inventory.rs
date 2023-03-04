0d15b1850113aa8344dd4e18061bbe0d3bc5a2d8
Carson McManus <carson.mcmanus1@gmail.com>
2023-03-04 13:40:51 -0500

    more doc comments

--- crates/valence/src/inventory.rs
+++ crates/valence/src/inventory.rs
@@ -1,3 +1,31 @@
+//! The inventory system.
+//!
+//! This module contains the systems and components needed to handle
+//! inventories. By default, clients will have a player inventory attached to
+//! them.
+//!
+//! # Components
+//!
+//! - [`Inventory`]: The inventory component. This is the thing that holds
+//!   items.
+//! - [`OpenInventory`]: The component that is attached to clients when they
+//!   have an inventory open.
+//!
+//! # Examples
+//!
+//! An example system that will let you access all player's inventories:
+//!
+//! ```rust
+//! # use valence::prelude::*;
+//! fn system(mut clients: Query<(&Client, &Inventory)>) {}
+//! ```
+//!
+//! ### See also
+//!
+//! Examples related to inventories in the `examples/` directory:
+//! - `building`
+//! - `chest`
+
 use std::borrow::Cow;
 use std::iter::FusedIterator;
 
