a0550d041b9b1c933abc1e4b9d41ed5ba741bcf9
AviiNL <me@avii.nl>
2023-03-11 12:12:50 +0100

    Abstract scoreboard display position with enum instead of raw byte

--- crates/valence_protocol/src/packet/s2c/play/scoreboard_display.rs
+++ crates/valence_protocol/src/packet/s2c/play/scoreboard_display.rs
@@ -2,6 +2,44 @@ use crate::{Decode, Encode};
 
 #[derive(Copy, Clone, Debug, Encode, Decode)]
 pub struct ScoreboardDisplayS2c<'a> {
-    pub position: u8,
+    pub position: Position,
     pub score_name: &'a str,
+}
+
+#[derive(Copy, Clone, PartialEq, Debug)]
+pub enum Position {
+    List,
+    Sidebar,
+    BelowName,
+    Team(u8),
+}
+
+impl Encode for Position {
+    fn encode(&self, w: impl std::io::Write) -> anyhow::Result<()> {
+        match self {
+            Position::List => 0u8.encode(w),
+            Position::Sidebar => 1u8.encode(w),
+            Position::BelowName => 2u8.encode(w),
+            Position::Team(value) => {
+                if value > &15 {
+                    return Err(anyhow::anyhow!("Invalid scoreboard display position"));
+                }
+
+                (3 + value).encode(w)
+            }
+        }
+    }
+}
+
+impl<'a> Decode<'a> for Position {
+    fn decode(r: &mut &'a [u8]) -> anyhow::Result<Self> {
+        let value = u8::decode(r)?;
+        match value {
+            0 => Ok(Position::List),
+            1 => Ok(Position::Sidebar),
+            2 => Ok(Position::BelowName),
+            3..=15 => Ok(Position::Team(value - 3)),
+            _ => Err(anyhow::anyhow!("Invalid scoreboard display position")),
+        }
+    }
 }
