68049e1e85c2ebef9922c99726d7d91233a3d62a
Ryan <ryanj00a@gmail.com>
2023-03-23 06:33:20 +0000

    Add README, run graphviz

--- crates/dump_schedule/src/main.rs
+++ crates/dump_schedule/src/main.rs
@@ -1,12 +1,16 @@
+use std::io;
+use std::io::Write;
+use std::process::{Command, Stdio};
+
 use valence::bevy_app::prelude::*;
 use valence::config::ServerPlugin;
 
-fn main() -> std::io::Result<()> {
+fn main() -> io::Result<()> {
     let mut app = App::new();
 
     app.add_plugin(ServerPlugin::new(()));
 
-    let data = bevy_mod_debugdump::schedule_graph_dot(
+    let dot_graph = bevy_mod_debugdump::schedule_graph_dot(
         &mut app,
         CoreSchedule::Main,
         &bevy_mod_debugdump::schedule_graph::Settings {
@@ -15,9 +19,18 @@ fn main() -> std::io::Result<()> {
         },
     );
 
-    let path = "graph.gv";
+    let mut child = Command::new("dot")
+        .stdin(Stdio::piped())
+        .arg("-Tsvg")
+        .arg("-o")
+        .arg("graph.svg")
+        .spawn()?;
 
-    println!("Writing schedule dump to file '{path}'");
+    if let Some(stdin) = child.stdin.as_mut() {
+        stdin.write_all(dot_graph.as_bytes())?;
+    }
 
-    std::fs::write(path, data)
+    child.wait_with_output()?;
+
+    Ok(())
 }
