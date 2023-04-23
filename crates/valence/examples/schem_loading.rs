58001c972a4d311e7137b13473609823e738d2d9
MrlnHi <marlonclaus2004@gmail.com>
2023-04-23 11:26:28 +0200

    Fix schem_loading example

--- crates/valence/examples/schem_loading.rs
+++ crates/valence/examples/schem_loading.rs
@@ -1,9 +1,6 @@
 use std::path::PathBuf;
 
 use clap::Parser;
-use valence::bevy_app::AppExit;
-use valence::client::despawn_disconnected_clients;
-use valence::client::event::default_event_handler;
 use valence::prelude::*;
 use valence_schem::Schematic;
 
@@ -16,68 +13,61 @@ struct Cli {
     path: PathBuf,
 }
 
+#[derive(Resource)]
+struct SchemRes(Schematic);
+
 pub fn main() {
     tracing_subscriber::fmt().init();
 
+    let Cli { path } = Cli::parse();
+    if !path.exists() {
+        eprintln!("File `{}` does not exist. Exiting.", path.display());
+        return;
+    } else if !path.is_file() {
+        eprintln!("`{}` is not a file. Exiting.", path.display());
+        return;
+    }
+    let schem = match Schematic::load(path) {
+        Ok(schem) => schem,
+        Err(err) => {
+            eprintln!("Error loading schematic: {err}");
+            return;
+        }
+    };
+
     App::new()
-        .add_plugin(ServerPlugin::new(()))
+        .add_plugins(DefaultPlugins)
+        .insert_resource(SchemRes(schem))
         .add_startup_system(setup)
-        .add_systems((
-            default_event_handler.in_schedule(EventLoopSchedule),
-            init_clients,
-            despawn_disconnected_clients,
-        ))
-        .add_systems(PlayerList::default_systems())
+        .add_systems((init_clients, despawn_disconnected_clients))
         .run();
 }
 
-fn setup(mut commands: Commands, server: Res<Server>, mut exit: EventWriter<AppExit>) {
-    let Cli { path } = Cli::parse();
-
-    if !path.exists() {
-        eprintln!("File `{}` does not exist. Exiting.", path.display());
-        exit.send_default();
-    } else if !path.is_file() {
-        eprintln!("`{}` is not a file. Exiting.", path.display());
-        exit.send_default();
-    }
-
-    let mut instance = server.new_instance(DimensionId::default());
-
-    match Schematic::load(path) {
-        Ok(schem) => {
-            schem.paste(&mut instance, SPAWN_POS, |_| BiomeId::default());
-        }
-        Err(err) => {
-            eprintln!("Error loading schematic: {err}");
-            exit.send_default();
-        }
-    }
-
+fn setup(
+    mut commands: Commands,
+    dimensions: Query<&DimensionType>,
+    biomes: Query<&Biome>,
+    server: Res<Server>,
+    schem: Res<SchemRes>,
+) {
+    let mut instance = Instance::new(ident!("overworld"), &dimensions, &biomes, &server);
+    schem
+        .0
+        .paste(&mut instance, SPAWN_POS, |_| BiomeId::default());
     commands.spawn(instance);
 }
 
 fn init_clients(
-    mut clients: Query<&mut Client, Added<Client>>,
+    mut clients: Query<(&mut Location, &mut Position, &mut GameMode), Added<Client>>,
     instances: Query<Entity, With<Instance>>,
-    mut commands: Commands,
 ) {
-    for mut client in &mut clients {
-        let instance = instances.single();
-
-        client.set_flat(true);
-        client.set_game_mode(GameMode::Creative);
-        client.set_position([
+    for (mut loc, mut pos, mut game_mode) in &mut clients {
+        *game_mode = GameMode::Creative;
+        pos.set([
             SPAWN_POS.x as f64 + 0.5,
             SPAWN_POS.y as f64,
             SPAWN_POS.z as f64 + 0.5,
         ]);
-        client.set_instance(instance);
-
-        commands.spawn(McEntity::with_uuid(
-            EntityKind::Player,
-            instance,
-            client.uuid(),
-        ));
+        loc.0 = instances.single();
     }
 }
