6ddf39f85969dfa14d8a2402147e3685476b1f46
Ryan <ryanj00a@gmail.com>
2023-04-23 02:44:12 +0000

    Add `idle_update` bench

--- crates/valence/benches/main.rs
+++ crates/valence/benches/main.rs
@@ -3,6 +3,7 @@ use criterion::{criterion_group, criterion_main};
 mod anvil;
 mod block;
 mod decode_array;
+mod idle;
 mod packet;
 mod var_int;
 mod var_long;
@@ -12,6 +13,7 @@ criterion_group! {
     anvil::load,
     block::block,
     decode_array::decode_array,
+    idle::idle_update,
     packet::packet,
     var_int::var_int,
     var_long::var_long,
