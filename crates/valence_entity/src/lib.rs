2e8fe3ad5edbb5593e754856c850cb2de24eac9f
Ryan <ryanj00a@gmail.com>
2023-05-29 00:23:37 -0700

    Fix more docs

--- crates/valence_entity/src/lib.rs
+++ crates/valence_entity/src/lib.rs
@@ -404,7 +404,7 @@ pub struct PacketByteRange(pub Range<usize>);
 /// Cache for all the tracked data of an entity. Used for the
 /// [`EntityTrackerUpdateS2c`][packet] packet.
 ///
-/// [packet]: crate::EntityTrackerUpdateS2c
+/// [packet]: crate::packet::EntityTrackerUpdateS2c
 #[derive(Component, Default, Debug)]
 pub struct TrackedData {
     init_data: Vec<u8>,
@@ -419,7 +419,7 @@ impl TrackedData {
     /// [`EntityTrackerUpdateS2c`][packet] packet. This is used when the entity
     /// enters the view of a client.
     ///
-    /// [packet]: valence_core::packet::s2c::play::EntityTrackerUpdateS2c
+    /// [packet]: crate::packet::EntityTrackerUpdateS2c
     pub fn init_data(&self) -> Option<&[u8]> {
         if self.init_data.len() > 1 {
             Some(&self.init_data)
@@ -432,7 +432,7 @@ impl TrackedData {
     /// [`EntityTrackerUpdateS2c`][packet] packet. This is used when tracked
     /// data is changed and the client is already in view of the entity.
     ///
-    /// [packet]: valence_core::packet::s2c::play::EntityTrackerUpdateS2c
+    /// [packet]: crate::packet::EntityTrackerUpdateS2c
     pub fn update_data(&self) -> Option<&[u8]> {
         if self.update_data.len() > 1 {
             Some(&self.update_data)
