71727755ecf93e2e48a23fd4884b17a1380fa0f3
tachibanayui <33594017+tachibanayui@users.noreply.github.com>
2023-06-17 13:40:23 +0700

    add crate level attributes

--- crates/valence_world_time/src/lib.rs
+++ crates/valence_world_time/src/lib.rs
@@ -70,6 +70,24 @@
 //!     }
 //! }
 //! ```
+#![allow(clippy::type_complexity)]
+#![deny(
+    rustdoc::broken_intra_doc_links,
+    rustdoc::private_intra_doc_links,
+    rustdoc::missing_crate_level_docs,
+    rustdoc::invalid_codeblock_attributes,
+    rustdoc::invalid_rust_codeblocks,
+    rustdoc::bare_urls,
+    rustdoc::invalid_html_tags
+)]
+#![warn(
+    trivial_casts,
+    trivial_numeric_casts,
+    unused_lifetimes,
+    unused_import_braces,
+    unreachable_pub,
+    clippy::dbg_macro
+)]
 
 pub mod packet;
 
