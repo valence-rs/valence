fbef5c346b40bf6bd465c75531f4b57cdd8fef12
tachibanayui <33594017+tachibanayui@users.noreply.github.com>
2023-06-21 14:59:16 +0700

    Update `valence_network` packets

--- crates/valence_network/src/packet.rs
+++ crates/valence_network/src/packet.rs
@@ -9,7 +9,7 @@ use valence_core::protocol::{packet_id, Decode, Encode, Packet};
 use valence_core::text::Text;
 
 #[derive(Clone, Debug, Encode, Decode, Packet)]
-#[packet(id = packet_id::HANDSHAKE_C2S)]
+#[packet(id = packet_id::HANDSHAKE_C2S, state = PacketState::Handshaking)]
 pub struct HandshakeC2s<'a> {
     pub protocol_version: VarInt,
     pub server_address: &'a str,
@@ -26,62 +26,62 @@ pub enum HandshakeNextState {
 }
 
 #[derive(Copy, Clone, Debug, Encode, Decode, Packet)]
-#[packet(id = packet_id::QUERY_PING_C2S)]
+#[packet(id = packet_id::QUERY_PING_C2S, state = PacketState::Status)]
 pub struct QueryPingC2s {
     pub payload: u64,
 }
 
 #[derive(Copy, Clone, Debug, Encode, Decode, Packet)]
-#[packet(id = packet_id::QUERY_REQUEST_C2S)]
+#[packet(id = packet_id::QUERY_REQUEST_C2S, state = PacketState::Status)]
 pub struct QueryRequestC2s;
 
 #[derive(Copy, Clone, Debug, Encode, Decode, Packet)]
-#[packet(id = packet_id::QUERY_PONG_S2C)]
+#[packet(id = packet_id::QUERY_PONG_S2C, state = PacketState::Status)]
 pub struct QueryPongS2c {
     pub payload: u64,
 }
 
 #[derive(Copy, Clone, Debug, Encode, Decode, Packet)]
-#[packet(id = packet_id::QUERY_RESPONSE_S2C)]
+#[packet(id = packet_id::QUERY_RESPONSE_S2C, state = PacketState::Status)]
 pub struct QueryResponseS2c<'a> {
     pub json: &'a str,
 }
 
 #[derive(Clone, Debug, Encode, Decode, Packet)]
-#[packet(id = packet_id::LOGIN_HELLO_C2S)]
+#[packet(id = packet_id::LOGIN_HELLO_C2S, state = PacketState::Login)]
 pub struct LoginHelloC2s<'a> {
     pub username: &'a str, // TODO: bound this
     pub profile_id: Option<Uuid>,
 }
 
 #[derive(Clone, Debug, Encode, Decode, Packet)]
-#[packet(id = packet_id::LOGIN_KEY_C2S)]
+#[packet(id = packet_id::LOGIN_KEY_C2S, state = PacketState::Login)]
 pub struct LoginKeyC2s<'a> {
     pub shared_secret: &'a [u8],
     pub verify_token: &'a [u8],
 }
 
 #[derive(Clone, Debug, Encode, Decode, Packet)]
-#[packet(id = packet_id::LOGIN_QUERY_RESPONSE_C2S)]
+#[packet(id = packet_id::LOGIN_QUERY_RESPONSE_C2S, state = PacketState::Login)]
 pub struct LoginQueryResponseC2s<'a> {
     pub message_id: VarInt,
     pub data: Option<RawBytes<'a>>,
 }
 
 #[derive(Copy, Clone, Debug, Encode, Decode, Packet)]
-#[packet(id = packet_id::LOGIN_COMPRESSION_S2C)]
+#[packet(id = packet_id::LOGIN_COMPRESSION_S2C, state = PacketState::Login)]
 pub struct LoginCompressionS2c {
     pub threshold: VarInt,
 }
 
 #[derive(Clone, Debug, Encode, Decode, Packet)]
-#[packet(id = packet_id::LOGIN_DISCONNECT_S2C)]
+#[packet(id = packet_id::LOGIN_DISCONNECT_S2C, state = PacketState::Login)]
 pub struct LoginDisconnectS2c<'a> {
     pub reason: Cow<'a, Text>,
 }
 
 #[derive(Copy, Clone, Debug, Encode, Decode, Packet)]
-#[packet(id = packet_id::LOGIN_HELLO_S2C)]
+#[packet(id = packet_id::LOGIN_HELLO_S2C, state = PacketState::Login)]
 pub struct LoginHelloS2c<'a> {
     pub server_id: &'a str,
     pub public_key: &'a [u8],
@@ -89,7 +89,7 @@ pub struct LoginHelloS2c<'a> {
 }
 
 #[derive(Clone, Debug, Encode, Decode, Packet)]
-#[packet(id = packet_id::LOGIN_QUERY_REQUEST_S2C)]
+#[packet(id = packet_id::LOGIN_QUERY_REQUEST_S2C, state = PacketState::Login)]
 pub struct LoginQueryRequestS2c<'a> {
     pub message_id: VarInt,
     pub channel: Ident<Cow<'a, str>>,
@@ -97,7 +97,7 @@ pub struct LoginQueryRequestS2c<'a> {
 }
 
 #[derive(Clone, Debug, Encode, Decode, Packet)]
-#[packet(id = packet_id::LOGIN_SUCCESS_S2C)]
+#[packet(id = packet_id::LOGIN_SUCCESS_S2C, state = PacketState::Login)]
 pub struct LoginSuccessS2c<'a> {
     pub uuid: Uuid,
     pub username: &'a str, // TODO: bound this.
