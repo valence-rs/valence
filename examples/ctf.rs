f8f70ee91018ea3227570001e9d019650e5fad72
Carson McManus <carson.mcmanus1@gmail.com>
2023-07-27 07:47:23 -0400

    ctf: first iteration

---
+++ examples/ctf.rs
@@ -0,0 +1,594 @@
+#![allow(clippy::type_complexity)]
+
+use valence::inventory::HeldItem;
+use valence::prelude::*;
+use valence_client::interact_block::InteractBlockEvent;
+use valence_client::message::SendMessage;
+
+const ARENA_Y: i32 = 64;
+const ARENA_MID_WIDTH: i32 = 2;
+const SPAWN_BOX: [i32; 3] = [0, ARENA_Y + 20, 0];
+const SPAWN_POS: [f64; 3] = [
+    SPAWN_BOX[0] as f64,
+    SPAWN_BOX[1] as f64 + 1.0,
+    SPAWN_BOX[2] as f64,
+];
+const SPAWN_BOX_WIDTH: i32 = 5;
+const SPAWN_BOX_HEIGHT: i32 = 4;
+
+pub fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup)
+        .add_systems(
+            Update,
+            (
+                init_clients,
+                despawn_disconnected_clients,
+                digging,
+                place_blocks,
+                do_team_selector_portals,
+                update_flag_visuals,
+                do_flag_capturing,
+                // visualize_triggers,
+            ),
+        )
+        .run();
+}
+
+fn setup(
+    mut commands: Commands,
+    server: Res<Server>,
+    dimensions: Res<DimensionTypeRegistry>,
+    biomes: Res<BiomeRegistry>,
+) {
+    let mut layer = LayerBundle::new(ident!("overworld"), &dimensions, &biomes, &server);
+
+    for z in -5..5 {
+        for x in -5..5 {
+            layer.chunk.insert_chunk([x, z], UnloadedChunk::new());
+        }
+    }
+
+    for z in -50..50 {
+        for x in -50..50 {
+            let block = match x {
+                x if x < -ARENA_MID_WIDTH => BlockState::RED_CONCRETE,
+                x if x > ARENA_MID_WIDTH => BlockState::BLUE_CONCRETE,
+                _ => BlockState::WHITE_CONCRETE,
+            };
+            layer.chunk.set_block([x, ARENA_Y, z], block);
+        }
+    }
+
+    let red_flag = build_flag(
+        &mut layer,
+        Team::Red,
+        BlockPos {
+            x: -48,
+            y: ARENA_Y + 1,
+            z: 0,
+        },
+    );
+    let blue_flag = build_flag(
+        &mut layer,
+        Team::Blue,
+        BlockPos {
+            x: 48,
+            y: ARENA_Y + 1,
+            z: 0,
+        },
+    );
+
+    build_spawn_box(&mut layer, SPAWN_BOX, &mut commands);
+
+    commands.spawn(layer);
+
+    let red_capture_trigger = TriggerArea::new(
+        red_flag - BlockPos::new(5, 3, 5),
+        red_flag + BlockPos::new(5, 3, 5),
+    );
+    let blue_capture_trigger = TriggerArea::new(
+        blue_flag - BlockPos::new(5, 3, 5),
+        blue_flag + BlockPos::new(5, 3, 5),
+    );
+    let mappos = CtfGlobals {
+        red_flag,
+        blue_flag,
+
+        red_capture_trigger,
+        blue_capture_trigger,
+    };
+
+    commands.insert_resource(mappos);
+    commands.insert_resource(FlagManager {
+        red: None,
+        blue: None,
+    })
+}
+
+/// Build a flag at the given position. `pos` should be the position of the
+/// bottom of the flag.
+///
+/// Returns the block position of the flag.
+fn build_flag(layer: &mut LayerBundle, team: Team, pos: impl Into<BlockPos>) -> BlockPos {
+    let mut pos = pos.into();
+
+    // build the flag pole
+    for _ in 0..3 {
+        layer.chunk.set_block(pos, BlockState::OAK_FENCE);
+        pos.y += 1;
+    }
+    let moving_east = pos.x < 0;
+    layer.chunk.set_block(
+        pos,
+        BlockState::OAK_FENCE.set(
+            if moving_east {
+                PropName::East
+            } else {
+                PropName::West
+            },
+            PropValue::True,
+        ),
+    );
+    pos.x += if pos.x < 0 { 1 } else { -1 };
+    layer.chunk.set_block(
+        pos,
+        BlockState::OAK_FENCE
+            .set(PropName::East, PropValue::True)
+            .set(PropName::West, PropValue::True),
+    );
+    pos.x += if pos.x < 0 { 1 } else { -1 };
+    layer.chunk.set_block(
+        pos,
+        BlockState::OAK_FENCE.set(
+            if moving_east {
+                PropName::West
+            } else {
+                PropName::East
+            },
+            PropValue::True,
+        ),
+    );
+    pos.y -= 1;
+
+    // build the flag
+    layer.chunk.set_block(
+        pos,
+        match team {
+            Team::Red => BlockState::RED_WOOL,
+            Team::Blue => BlockState::BLUE_WOOL,
+        },
+    );
+
+    return pos;
+}
+
+fn build_spawn_box(layer: &mut LayerBundle, pos: impl Into<BlockPos>, commands: &mut Commands) {
+    let pos = pos.into();
+
+    let spawn_box_block = BlockState::GLASS;
+
+    // build floor and roof
+    for z in -SPAWN_BOX_WIDTH..=SPAWN_BOX_WIDTH {
+        for x in -SPAWN_BOX_WIDTH..=SPAWN_BOX_WIDTH {
+            layer
+                .chunk
+                .set_block([pos.x + x, pos.y, pos.z + z], spawn_box_block);
+            layer.chunk.set_block(
+                [pos.x + x, pos.y + SPAWN_BOX_HEIGHT, pos.z + z],
+                spawn_box_block,
+            );
+        }
+    }
+
+    // build walls
+    for z in [-SPAWN_BOX_WIDTH, SPAWN_BOX_WIDTH] {
+        for x in -SPAWN_BOX_WIDTH..=SPAWN_BOX_WIDTH {
+            for y in pos.y..=pos.y + SPAWN_BOX_HEIGHT - 1 {
+                layer
+                    .chunk
+                    .set_block([pos.x + x, y, pos.z + z], spawn_box_block);
+            }
+        }
+    }
+
+    for x in [-SPAWN_BOX_WIDTH, SPAWN_BOX_WIDTH] {
+        for z in -SPAWN_BOX_WIDTH..=SPAWN_BOX_WIDTH {
+            for y in pos.y..=pos.y + SPAWN_BOX_HEIGHT - 1 {
+                layer
+                    .chunk
+                    .set_block([pos.x + x, y, pos.z + z], spawn_box_block);
+            }
+        }
+    }
+
+    // build team selector portals
+    for (block, offset) in [
+        (
+            BlockState::RED_CONCRETE,
+            BlockPos::new(-SPAWN_BOX_WIDTH, 0, SPAWN_BOX_WIDTH - 2),
+        ),
+        (
+            BlockState::BLUE_CONCRETE,
+            BlockPos::new(SPAWN_BOX_WIDTH - 2, 0, SPAWN_BOX_WIDTH - 2),
+        ),
+    ] {
+        for z in 0..3 {
+            for x in 0..3 {
+                layer.chunk.set_block(
+                    [pos.x + offset.x + x, pos.y + offset.y, pos.z + offset.z + z],
+                    block,
+                );
+            }
+        }
+    }
+
+    let red = [
+        pos.x - SPAWN_BOX_WIDTH + 1,
+        pos.y,
+        pos.z + SPAWN_BOX_WIDTH - 1,
+    ];
+    let red_area = TriggerArea::new(red, red);
+    let blue = [
+        pos.x + SPAWN_BOX_WIDTH - 1,
+        pos.y,
+        pos.z + SPAWN_BOX_WIDTH - 1,
+    ];
+    let blue_area = TriggerArea::new(blue, blue);
+    let portals = Portals {
+        red: red_area,
+        blue: blue_area,
+    };
+
+    for pos in portals.red.iter_block_pos() {
+        layer.chunk.set_block(pos, BlockState::AIR);
+    }
+    for pos in portals.blue.iter_block_pos() {
+        layer.chunk.set_block(pos, BlockState::AIR);
+    }
+
+    layer
+        .chunk
+        .set_block(portals.red.a - BlockPos::new(0, 1, 0), BlockState::BARRIER);
+    layer
+        .chunk
+        .set_block(portals.blue.a - BlockPos::new(0, 1, 0), BlockState::BARRIER);
+
+    commands.insert_resource(portals);
+}
+
+fn init_clients(
+    mut clients: Query<
+        (
+            &mut Client,
+            &mut EntityLayerId,
+            &mut VisibleChunkLayer,
+            &mut VisibleEntityLayers,
+            &mut Position,
+            &mut GameMode,
+        ),
+        Added<Client>,
+    >,
+    layers: Query<Entity, (With<ChunkLayer>, With<EntityLayer>)>,
+) {
+    for (
+        mut client,
+        mut layer_id,
+        mut visible_chunk_layer,
+        mut visible_entity_layers,
+        mut pos,
+        mut game_mode,
+    ) in &mut clients
+    {
+        let layer = layers.single();
+
+        layer_id.0 = layer;
+        visible_chunk_layer.0 = layer;
+        visible_entity_layers.0.insert(layer);
+        pos.set(SPAWN_POS);
+        *game_mode = GameMode::Adventure;
+
+        client.send_chat_message("Welcome to Valence! Select a team.".italic());
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, Component)]
+enum Team {
+    Red,
+    Blue,
+}
+
+impl Team {
+    pub fn spawn_pos(&self) -> DVec3 {
+        [
+            match self {
+                Team::Red => -40.0,
+                Team::Blue => 40.0,
+            },
+            ARENA_Y as f64 + 1.0,
+            0.0,
+        ]
+        .into()
+    }
+
+    pub fn team_text(&self) -> Text {
+        match self {
+            Team::Red => "RED".color(Color::RED).bold(),
+            Team::Blue => "BLUE".color(Color::BLUE).bold(),
+        }
+    }
+}
+
+fn digging(
+    mut clients: Query<(&GameMode, &Team, Entity, &mut Client)>,
+    mut layers: Query<&mut ChunkLayer>,
+    mut events: EventReader<DiggingEvent>,
+    mut commands: Commands,
+    globals: Res<CtfGlobals>,
+    mut flag_manager: ResMut<FlagManager>,
+) {
+    let mut layer = layers.single_mut();
+
+    for event in events.iter() {
+        let Ok((game_mode, team, ent, mut client)) = clients.get_mut(event.client) else {
+            continue;
+        };
+
+        if (*game_mode == GameMode::Creative && event.state == DiggingState::Start)
+            || (*game_mode == GameMode::Survival && event.state == DiggingState::Stop)
+        {
+            let Some(block) = layer.block(event.position) else {
+                continue;
+            };
+            let is_flag = event.position == globals.red_flag || event.position == globals.blue_flag;
+
+            match (team, block.state) {
+                (Team::Blue, BlockState::RED_WOOL) => {
+                    if event.position == globals.red_flag {
+                        commands.entity(event.client).insert(HasFlag(Team::Red));
+                        client.send_chat_message("You have the flag!".italic());
+                        flag_manager.red = Some(ent);
+                        return;
+                    }
+                }
+                (Team::Red, BlockState::BLUE_WOOL) => {
+                    if event.position == globals.blue_flag {
+                        commands.entity(event.client).insert(HasFlag(Team::Blue));
+                        client.send_chat_message("You have the flag!".italic());
+                        flag_manager.blue = Some(ent);
+                        return;
+                    }
+                }
+                _ => {}
+            }
+
+            if event.position.y <= ARENA_Y || block.state == BlockState::OAK_FENCE || is_flag {
+                continue;
+            }
+
+            layer.set_block(event.position, BlockState::AIR);
+        }
+    }
+}
+
+fn place_blocks(
+    mut clients: Query<(&mut Inventory, &GameMode, &HeldItem)>,
+    mut layers: Query<&mut ChunkLayer>,
+    mut events: EventReader<InteractBlockEvent>,
+) {
+    let mut layer = layers.single_mut();
+
+    for event in events.iter() {
+        let Ok((mut inventory, game_mode, held)) = clients.get_mut(event.client) else {
+            continue;
+        };
+        if event.hand != Hand::Main {
+            continue;
+        }
+
+        // get the held item
+        let slot_id = held.slot();
+        let Some(stack) = inventory.slot(slot_id) else {
+            // no item in the slot
+            continue;
+        };
+
+        let Some(block_kind) = BlockKind::from_item_kind(stack.item) else {
+            // can't place this item as a block
+            continue;
+        };
+
+        if *game_mode == GameMode::Survival {
+            // check if the player has the item in their inventory and remove
+            // it.
+            if stack.count() > 1 {
+                let count = stack.count();
+                inventory.set_slot_amount(slot_id, count - 1);
+            } else {
+                inventory.set_slot(slot_id, None);
+            }
+        }
+        let real_pos = event.position.get_in_direction(event.face);
+        layer.set_block(real_pos, block_kind.to_state());
+    }
+}
+
+#[derive(Debug, Resource)]
+struct Portals {
+    red: TriggerArea,
+    blue: TriggerArea,
+}
+
+fn do_team_selector_portals(
+    mut players: Query<(Entity, &mut Position, &mut GameMode, &mut Client), Without<Team>>,
+    portals: Res<Portals>,
+    mut commands: Commands,
+) {
+    for player in players.iter_mut() {
+        let (player, mut pos, mut game_mode, mut client) = player;
+        if pos.0.y < SPAWN_BOX[1] as f64 - 5.0 {
+            pos.0 = SPAWN_POS.into();
+            continue;
+        }
+
+        let team = if portals.red.contains_pos(pos.0) {
+            Some(Team::Red)
+        } else if portals.blue.contains_pos(pos.0) {
+            Some(Team::Blue)
+        } else {
+            None
+        };
+
+        if let Some(team) = team {
+            *game_mode = GameMode::Survival;
+            commands.entity(player).insert(team);
+            pos.0 = team.spawn_pos();
+            let chat_text: Text = "You are on team ".into_text() + team.team_text() + "!";
+            client.send_chat_message(chat_text);
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+struct TriggerArea {
+    pub a: BlockPos,
+    pub b: BlockPos,
+}
+
+impl TriggerArea {
+    pub fn new(a: impl Into<BlockPos>, b: impl Into<BlockPos>) -> Self {
+        Self {
+            a: a.into(),
+            b: b.into(),
+        }
+    }
+
+    pub fn contains(&self, pos: BlockPos) -> bool {
+        let min = BlockPos::new(
+            self.a.x.min(self.b.x),
+            self.a.y.min(self.b.y),
+            self.a.z.min(self.b.z),
+        );
+        let max = BlockPos::new(
+            self.a.x.max(self.b.x),
+            self.a.y.max(self.b.y),
+            self.a.z.max(self.b.z),
+        );
+
+        pos.x >= min.x
+            && pos.x <= max.x
+            && pos.y >= min.y
+            && pos.y <= max.y
+            && pos.z >= min.z
+            && pos.z <= max.z
+    }
+
+    pub fn contains_pos(&self, pos: DVec3) -> bool {
+        self.contains(BlockPos::from_pos(pos))
+    }
+
+    pub fn iter_block_pos(&self) -> impl Iterator<Item = BlockPos> {
+        let min = BlockPos::new(
+            self.a.x.min(self.b.x),
+            self.a.y.min(self.b.y),
+            self.a.z.min(self.b.z),
+        );
+        let max = BlockPos::new(
+            self.a.x.max(self.b.x),
+            self.a.y.max(self.b.y),
+            self.a.z.max(self.b.z),
+        );
+
+        (min.x..=max.x)
+            .flat_map(move |x| (min.y..=max.y).map(move |y| (x, y)))
+            .flat_map(move |(x, y)| (min.z..=max.z).map(move |z| BlockPos::new(x, y, z)))
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, Component)]
+#[component(storage = "SparseSet")]
+struct HasFlag(Team);
+
+#[derive(Debug, Resource)]
+struct FlagManager {
+    red: Option<Entity>,
+    blue: Option<Entity>,
+}
+
+#[derive(Debug, Resource)]
+struct CtfGlobals {
+    pub red_flag: BlockPos,
+    pub blue_flag: BlockPos,
+
+    pub red_capture_trigger: TriggerArea,
+    pub blue_capture_trigger: TriggerArea,
+}
+
+fn update_flag_visuals(
+    flag_manager: Res<FlagManager>,
+    globals: Res<CtfGlobals>,
+    mut layers: Query<&mut ChunkLayer>,
+) {
+    if !flag_manager.is_changed() {
+        return;
+    }
+    let red_flag_block = match flag_manager.red {
+        Some(_) => BlockState::AIR,
+        None => BlockState::RED_WOOL,
+    };
+    let blue_flag_block = match flag_manager.blue {
+        Some(_) => BlockState::AIR,
+        None => BlockState::BLUE_WOOL,
+    };
+
+    layers
+        .single_mut()
+        .set_block(globals.red_flag, red_flag_block);
+    layers
+        .single_mut()
+        .set_block(globals.blue_flag, blue_flag_block);
+}
+
+fn do_flag_capturing(
+    globals: Res<CtfGlobals>,
+    mut players: Query<(Entity, &mut Client, &Team, &Position, &HasFlag)>,
+    mut commands: Commands,
+    mut flag_manager: ResMut<FlagManager>,
+) {
+    for (ent, mut client, team, position, has_flag) in players.iter_mut() {
+        let capture_trigger = match team {
+            Team::Red => &globals.red_capture_trigger,
+            Team::Blue => &globals.blue_capture_trigger,
+        };
+
+        if capture_trigger.contains_pos(position.0) {
+            client.send_chat_message("You captured the flag!".italic());
+            commands.entity(ent).remove::<HasFlag>();
+            match has_flag.0 {
+                Team::Red => flag_manager.red = None,
+                Team::Blue => flag_manager.blue = None,
+            }
+        }
+    }
+}
+
+fn visualize_triggers(globals: Res<CtfGlobals>, mut layers: Query<&mut ChunkLayer>) {
+    fn vis_trigger(trigger: &TriggerArea, layer: &mut ChunkLayer) {
+        for pos in trigger.iter_block_pos() {
+            layer.play_particle(
+                &Particle::Crit,
+                false,
+                [pos.x as f64 + 0.5, pos.y as f64 + 0.5, pos.z as f64 + 0.5],
+                [0., 0., 0.],
+                0.0,
+                1,
+            );
+        }
+    }
+
+    for mut layer in layers.iter_mut() {
+        vis_trigger(&globals.red_capture_trigger, &mut layer);
+        vis_trigger(&globals.blue_capture_trigger, &mut layer);
+    }
+}
