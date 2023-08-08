acf5536619e4b41ebc06f6bed14e069ad461249a
pepperoni21 <pepperoni2100@gmail.com>
2023-08-08 14:34:00 +0200

    Edited comments

--- src/tests/weather.rs
+++ src/tests/weather.rs
@@ -14,7 +14,7 @@ fn test_client_initialization_on_join() {
 
     app.update();
 
-    // Check if a world border initialize packet was sent
+    // Check if two game state change packets were sent, one for rain and one for thunder
     let frames = helper.collect_received();
     frames.assert_count::<GameStateChangeS2c>(2);
 }
@@ -30,7 +30,7 @@ fn test_chunk_layer_initialization_on_join() {
 
     app.update();
 
-    // Check if a world border initialize packet was sent
+    // Check if two game state change packets were sent, one for rain and one for thunder
     let frames = helper.collect_received();
     frames.assert_count::<GameStateChangeS2c>(2);
 }
@@ -48,12 +48,13 @@ fn test_client_rain_change() {
 
     helper.clear_received();
 
+    // Change the rain value
     let mut rain = app.world.get_mut::<Rain>(client).unwrap();
     rain.0 = 1.0;
 
     app.update();
 
-    // Check if a world border initialize packet was sent
+    // Check if a game state change packet was sent
     let frames = helper.collect_received();
     frames.assert_count::<GameStateChangeS2c>(1);
 }
@@ -71,12 +72,13 @@ fn test_client_thunder_change() {
 
     helper.clear_received();
 
+    // Change the thunder value
     let mut thunder = app.world.get_mut::<Thunder>(client).unwrap();
     thunder.0 = 1.0;
 
     app.update();
 
-    // Check if a world border initialize packet was sent
+    // Check if a game state change packet was sent
     let frames = helper.collect_received();
     frames.assert_count::<GameStateChangeS2c>(1);
 }
@@ -94,12 +96,13 @@ fn test_chunk_layer_rain_change() {
 
     helper.clear_received();
 
+    // Change the rain value
     let mut rain = app.world.get_mut::<Rain>(layer).unwrap();
     rain.0 = 1.0;
 
     app.update();
 
-    // Check if a world border initialize packet was sent
+    // Check if a game state change packet was sent
     let frames = helper.collect_received();
     frames.assert_count::<GameStateChangeS2c>(1);
 }
@@ -117,12 +120,13 @@ fn test_chunk_layer_thunder_change() {
 
     helper.clear_received();
 
+    // Change the thunder value
     let mut thunder = app.world.get_mut::<Thunder>(layer).unwrap();
     thunder.0 = 1.0;
 
     app.update();
 
-    // Check if a world border initialize packet was sent
+    // Check if a game state change packet was sent
     let frames = helper.collect_received();
     frames.assert_count::<GameStateChangeS2c>(1);
 }
@@ -133,6 +137,7 @@ fn prepare(client_weather: bool) -> ScenarioSingleClient {
     // Process a tick to get past the "on join" logic.
     s.app.update();
 
+    // Add weather to either the client or the chunk layer depending on the parameter
     if client_weather {
         add_weather_to_client(&mut s);
     } else {
