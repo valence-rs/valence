666e8af5d9dcf543891857c0796470be68a56b5f
icrayix <icrayix@gmail.com>
2023-08-23 16:16:49 +0200

    Improve buildscript

--- website/build.sh
+++ website/build.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env sh
 
 # This script builds the website and outputs ready-to-serve contents in ./public/
-# This script must be run from this directory.
 #
 # Requirements:
 # - zola       https://www.getzola.org/documentation/getting-started/installation/
 # - cargo      https://doc.rust-lang.org/cargo/getting-started/installation.html
 # - mdBook     https://rust-lang.github.io/mdBook/guide/installation.html
+
+set -euo pipefail
+
+# cd into this directory
+cd "$(dirname "$0")"
 
 # clean the ./public directory
 rm -rf public/*
