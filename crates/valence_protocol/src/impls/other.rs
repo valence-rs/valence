1ddde71963ca3b15e8d97d1e4383b61431a30bbc
Ryan Johnson <ryanj00a@gmail.com>
2023-09-05 18:06:07 -0700

    NBT Tweaks (#522)
    # Objective
    
    `valence_nbt` could use some adjustments.
    
    # Solution
    
    - Move top-level conversion functions to `conv` module.
    - Remove public `Display` impl from `Tag`.
    - Remove implicit "null" byte check when decoding compounds. This check
    can be done in advance if desired.
    - Remove potentially dangerous memory preallocation when decoding
    binary.
    - Make `Compound::to_binary` and `Compound::from_binary` free functions
    for discoverability and familiarity with serde_json.

--- crates/valence_protocol/src/impls/other.rs
+++ crates/valence_protocol/src/impls/other.rs
@@ -44,15 +44,21 @@ impl<'a> Decode<'a> for Uuid {
 
 impl Encode for Compound {
     fn encode(&self, w: impl Write) -> anyhow::Result<()> {
-        Ok(self.to_binary(w, "")?)
+        Ok(valence_nbt::to_binary(self, w, "")?)
     }
 }
 
 impl Decode<'_> for Compound {
     fn decode(r: &mut &[u8]) -> anyhow::Result<Self> {
-        // TODO: Bound the input slice or do something else to prevent this from eating
-        // too much memory.
-        Ok(Self::from_binary(r)?.0)
+        // Check for null compound.
+        if r.first() == Some(&0) {
+            *r = &r[1..];
+            return Ok(Compound::new());
+        }
+
+        // TODO: consider if we need to bound the input slice or add some other
+        // mitigation to prevent excessive memory usage on hostile input.
+        Ok(valence_nbt::from_binary(r)?.0)
     }
 }
 
