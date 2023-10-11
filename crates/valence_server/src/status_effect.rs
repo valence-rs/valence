e58c3e590f197ef6ae75783fd021b637ccc8fa4f
SelfMadeSystem <sms@shoghisimon.cc>
2023-10-10 21:19:10 -0400

    Added swirls

--- crates/valence_server/src/status_effect.rs
+++ crates/valence_server/src/status_effect.rs
@@ -2,6 +2,7 @@ use bevy_app::prelude::*;
 use bevy_ecs::prelude::*;
 use valence_entity::active_status_effects::{ActiveStatusEffect, ActiveStatusEffects};
 use valence_entity::entity::Flags;
+use valence_entity::living::{PotionSwirlsAmbient, PotionSwirlsColor};
 use valence_entity::status_effects::StatusEffect;
 use valence_protocol::packets::play::{
     entity_status_effect_s2c, EntityStatusEffectS2c, RemoveEntityStatusEffectS2c,
@@ -59,9 +60,18 @@ fn add_status_effects(
         &mut ActiveStatusEffects,
         Option<&mut Client>,
         Option<&mut Flags>,
+        Option<&mut PotionSwirlsColor>,
+        Option<&mut PotionSwirlsAmbient>,
     )>,
 ) {
-    for (mut active_status_effects, mut client, mut entity_flags) in query.iter_mut() {
+    for (
+        mut active_status_effects,
+        mut client,
+        mut entity_flags,
+        mut swirl_color,
+        mut swirl_ambient,
+    ) in query.iter_mut()
+    {
         for new_effect in active_status_effects.move_new_to_active() {
             let status_effect = new_effect.status_effect();
 
@@ -73,8 +83,17 @@ fn add_status_effects(
                 set_entity_flags(status_effect, entity_flags, true);
             }
 
-            // TODO: More stuff such as particles, instant health, instant
-            // damage, etc.
+            if let Some(ref mut swirl_color) = swirl_color {
+                // TODO: Mix colors.
+                // See net.minecraft.potion.PotionUtil#getColor lines 90-115
+                swirl_color.0 = status_effect.color() as i32; // TODO: `as i32`????
+            }
+
+            if let Some(ref mut swirl_ambient) = swirl_ambient {
+                swirl_ambient.0 = new_effect.ambient();
+            }
+
+            // TODO: More stuff such as instant health, instant damage, etc.
 
             /* TODO: These things require to modify entity attributes:
              * - speed
