0e5f7cf4484965cb60f7383cfd47bf13238f039d
Sese Mueller <sese4dasbinichagmail.com>
2023-10-13 12:57:13 +0200

    Changed and commented according to suggestions

--- crates/valence_server/src/brand.rs
+++ crates/valence_server/src/brand.rs
@@ -1,5 +1,5 @@
 use valence_protocol::packets::play::CustomPayloadS2c;
-use valence_protocol::{ident, Bounded, WritePacket};
+use valence_protocol::{ident, Bounded, Encode, VarInt, WritePacket};
 
 pub trait SetBrand {
     /// Sets the brand of the server.
@@ -7,22 +7,22 @@ pub trait SetBrand {
     /// The Brand is displayed to the client in the F3 screen
     /// and is often used to display the server name.
     /// Any valid &str can be used.
+    ///
+    /// However, the legacy formatting codes are used,
+    /// which means that while color and other formatting can technically be
+    /// used, it needs to use the § character, which needs to be encoded as
+    /// 0xC2, 0xA7.
     fn set_brand(&mut self, brand: &str);
 }
 
 impl<T: WritePacket> SetBrand for T {
     fn set_brand(&mut self, brand: &str) {
-        // The data is a &[u8], where the first byte is the length of the string.
-        // It is also UTF-8 encoded, so any valid &str can be used.
-        let vec_data = [brand.len() as u8]
-            .iter()
-            .chain(brand.as_bytes().iter())
-            .copied()
-            .collect::<Vec<_>>();
-        let data = vec_data.as_slice();
+        let mut buf = vec![];
+        let _ = VarInt(brand.len() as _).encode(&mut buf);
+        buf.extend_from_slice(brand.as_bytes());
         self.write_packet(&CustomPayloadS2c {
             channel: ident!("minecraft:brand").into(),
-            data: Bounded(data.into()),
+            data: Bounded(buf.as_slice().into()),
         });
     }
 }
