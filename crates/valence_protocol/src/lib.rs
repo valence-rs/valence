7d8dc774236eb9470eb8e1f8a995d3d78fa3c04d
Ryan Johnson <ryanj00a@gmail.com>
2024-03-03 16:06:38 +0000

    Expand `McRead` and `McWrite` traits

--- crates/valence_protocol/src/lib.rs
+++ crates/valence_protocol/src/lib.rs
@@ -1,4 +1,5 @@
-use std::io::Write;
+use std::io::{Cursor, Write};
+use std::mem::{self, MaybeUninit};
 
 use anyhow::{anyhow, bail, ensure, Context};
 use num_traits::{FromPrimitive, ToPrimitive};
@@ -51,30 +52,31 @@ pub struct Clientbound;
 impl PacketSide for Clientbound {}
 
 pub trait McWrite {
-    /// Write a slice of bytes directly to the output without any length prefix.
+    /// Write a slice of bytes directly to the output without a length prefix.
     fn write_bytes(&mut self, bytes: &[u8]) -> anyhow::Result<()>;
 
-    /// Write a variable length integer.
-    fn write_var_int(&mut self, int: i32) -> anyhow::Result<()> {
-        var_int::write_var_int(int, |b| self.write_bytes(&[b]))
+    fn write_byte_slice(&mut self, bytes: &[u8]) -> anyhow::Result<()> {
+        let len: i32 = bytes
+            .len()
+            .try_into()
+            .context("byte slice length exceeds `i32::MAX`")?;
+
+        self.write_var_int(len)?;
+        self.write_bytes(bytes)
     }
 
-    fn write_var_long(&mut self, int: i64) -> anyhow::Result<()> {
-        var_int::write_var_long(int, |b| self.write_bytes(&[b]))
-    }
-
-    /// Write a `str`. Errors if the string is longer than 32767 in utf-16
-    /// chars.
+    /// Write a `str`. Errors if the string is longer than
+    /// [`DEFAULT_MAX_STRING_LENGTH`] in UTF-16 chars.
     fn write_str(&mut self, str: &str) -> anyhow::Result<()> {
-        self.write_str_bounded(str, 32767)
+        self.write_str_bounded(str, DEFAULT_MAX_STRING_LENGTH)
     }
 
-    fn write_str_bounded(&mut self, str: &str, max_len: u32) -> anyhow::Result<()> {
+    fn write_str_bounded(&mut self, str: &str, max_char_count: u32) -> anyhow::Result<()> {
         let char_count = str.encode_utf16().count();
 
         ensure!(
-            char_count <= max_len as usize,
-            "utf-16 char count of string ({char_count}) exceeds maximum of {max_len}"
+            char_count <= max_char_count as usize,
+            "utf-16 char count of string ({char_count}) exceeds maximum of {max_char_count}"
         );
 
         let len: i32 = str
@@ -86,19 +88,70 @@ pub trait McWrite {
         self.write_bytes(str.as_bytes())
     }
 
-    fn write_enum<T: ToPrimitive>(&mut self, t: &T) -> anyhow::Result<()> {
-        self.write_var_int(t.to_i32().context("enum not representable as `i32`")?)
+    fn write_enum<T: ToPrimitive>(&mut self, val: &T) -> anyhow::Result<()> {
+        self.write_var_int(val.to_i32().context("enum not representable as `i32`")?)
+    }
+
+    /// Write a variable length integer.
+    fn write_var_int(&mut self, int: i32) -> anyhow::Result<()> {
+        var_int::write_var_int(int, |b| self.write_bytes(&[b]))
+    }
+
+    fn write_var_long(&mut self, int: i64) -> anyhow::Result<()> {
+        var_int::write_var_long(int, |b| self.write_bytes(&[b]))
+    }
+
+    fn write_u8(&mut self, int: u8) -> anyhow::Result<()> {
+        self.write_bytes(&[int])
     }
 
     fn write_i8(&mut self, int: i8) -> anyhow::Result<()> {
         self.write_bytes(&[int as u8])
     }
 
+    fn write_u16(&mut self, int: u16) -> anyhow::Result<()> {
+        self.write_bytes(&int.to_be_bytes())
+    }
+
+    fn write_i16(&mut self, int: i16) -> anyhow::Result<()> {
+        self.write_bytes(&int.to_be_bytes())
+    }
+
+    fn write_u32(&mut self, int: u32) -> anyhow::Result<()> {
+        self.write_bytes(&int.to_be_bytes())
+    }
+
+    fn write_i32(&mut self, int: i32) -> anyhow::Result<()> {
+        self.write_bytes(&int.to_be_bytes())
+    }
+
+    fn write_u64(&mut self, int: u64) -> anyhow::Result<()> {
+        self.write_bytes(&int.to_be_bytes())
+    }
+
     fn write_i64(&mut self, int: i64) -> anyhow::Result<()> {
+        self.write_bytes(&int.to_be_bytes())
+    }
+
+    fn write_u128(&mut self, int: u128) -> anyhow::Result<()> {
+        self.write_bytes(&int.to_be_bytes())
+    }
+
+    fn write_i128(&mut self, int: i128) -> anyhow::Result<()> {
+        self.write_bytes(&int.to_be_bytes())
+    }
+
+    fn write_f32(&mut self, int: f32) -> anyhow::Result<()> {
+        self.write_bytes(&int.to_be_bytes())
+    }
+
+    fn write_f64(&mut self, int: f64) -> anyhow::Result<()> {
         self.write_bytes(&int.to_be_bytes())
     }
 }
 
+/// A source of contiguous bytes for Minecraft packets to read from. The
+/// interface is similar to Minecraft's `PacketByteBuf` class.
 pub trait McRead<'a> {
     fn read_bytes(&mut self, count: usize) -> anyhow::Result<&'a [u8]>;
 
@@ -106,16 +159,69 @@ pub trait McRead<'a> {
         Ok(self.read_bytes(N)?.try_into().expect("invalid slice len"))
     }
 
-    fn read_str(&mut self) -> anyhow::Result<&'a str> {
-        self.read_str_bounded(32767)
+    /// Read an array of values from the input. The array is not length
+    /// prefixed.
+    fn read_array<T, const N: usize, F>(&mut self, mut f: F) -> anyhow::Result<[T; N]>
+    where
+        F: FnMut(&mut Self) -> anyhow::Result<T>,
+    {
+        // TODO: use std::array::try_from_fn when stabilized.
+
+        struct Guard<T, const N: usize> {
+            array: [MaybeUninit<T>; N],
+            initialized: usize,
+        }
+
+        impl<T, const N: usize> Drop for Guard<T, N> {
+            fn drop(&mut self) {
+                unsafe { std::ptr::drop_in_place(self.array.as_mut_slice()) };
+            }
+        }
+
+        // This is what `ArrayVec::new` does.
+        let initial_array: [MaybeUninit<T>; N] = unsafe { MaybeUninit::uninit().assume_init() };
+
+        let mut guard = Guard {
+            array: initial_array,
+            initialized: 0,
+        };
+
+        while guard.initialized < N {
+            *unsafe { guard.array.get_unchecked_mut(guard.initialized) } =
+                MaybeUninit::new(f(self)?);
+
+            guard.initialized += 1;
+        }
+
+        let res: [T; N] = unsafe { std::mem::transmute_copy(&guard.array) };
+
+        std::mem::forget(guard);
+
+        Ok(res)
     }
 
-    fn read_str_bounded(&mut self, max_len: u32) -> anyhow::Result<&'a str> {
-        let len = self.read_var_int()?;
-        let len: u32 = len.try_into().context("negative string length")?;
+    /// Read a varint-prefixed slice of bytes. There is no length limit.
+    fn read_byte_slice(&mut self) -> anyhow::Result<&'a [u8]> {
+        let len: u32 = self
+            .read_var_int()?
+            .try_into()
+            .context("negative byte slice length")?;
 
-        let max_bytes_per_char = 2;
-        let max_byte_len = max_len * max_bytes_per_char;
+        self.read_bytes(len as usize)
+    }
+
+    fn read_str(&mut self) -> anyhow::Result<&'a str> {
+        self.read_str_bounded(DEFAULT_MAX_STRING_LENGTH)
+    }
+
+    fn read_str_bounded(&mut self, max_char_count: u32) -> anyhow::Result<&'a str> {
+        let len: u32 = self
+            .read_var_int()?
+            .try_into()
+            .context("negative string length")?;
+
+        let max_bytes_per_char = 4;
+        let max_byte_len = max_char_count * max_bytes_per_char;
 
         ensure!(
             len <= max_byte_len,
@@ -124,11 +230,11 @@ pub trait McRead<'a> {
 
         let str = std::str::from_utf8(self.read_bytes(len as usize)?)?;
 
-        let utf16_len = str.encode_utf16().count();
+        let utf16_char_count = str.encode_utf16().count();
 
         ensure!(
-            utf16_len <= max_len as usize,
-            "utf-16 string length of {utf16_len} exceeds maximum of {max_len}"
+            utf16_char_count <= max_char_count as usize,
+            "UTF-16 string length of {utf16_char_count} exceeds maximum of {max_char_count}"
         );
 
         Ok(str)
@@ -137,6 +243,38 @@ pub trait McRead<'a> {
     fn read_enum<T: FromPrimitive>(&mut self) -> anyhow::Result<T> {
         let discriminant = self.read_var_int()?;
         T::from_i32(discriminant).context("failed to decode enum")
+    }
+
+    fn read_option<T, F>(&mut self, f: F) -> anyhow::Result<Option<T>>
+    where
+        F: FnOnce(&mut Self) -> anyhow::Result<T>,
+    {
+        self.read_bool()?.then(|| f(self)).transpose()
+    }
+
+    fn read_collection<C, T, CF, F>(&mut self, cf: CF, mut f: F) -> anyhow::Result<C>
+    where
+        C: Extend<T>,
+        CF: FnOnce(usize) -> C,
+        F: FnMut(&mut Self) -> anyhow::Result<T>,
+    {
+        let len = self.read_var_int()?;
+        let len: usize = len.try_into().context("invalid collection length")?;
+
+        let mut collection = cf(cautious_capacity::<T>(len));
+
+        for _ in 0..len {
+            collection.extend([f(self)?]);
+        }
+
+        Ok(collection)
+    }
+
+    fn read_vec<T, F>(&mut self, f: F) -> anyhow::Result<Vec<T>>
+    where
+        F: FnMut(&mut Self) -> anyhow::Result<T>,
+    {
+        self.read_collection(Vec::with_capacity, f)
     }
 
     fn read_var_int(&mut self) -> anyhow::Result<i32> {
@@ -155,23 +293,64 @@ pub trait McRead<'a> {
         }
     }
 
+    fn read_bool(&mut self) -> anyhow::Result<bool> {
+        let byte = self.read_u8()?;
+        ensure!(byte <= 1, "boolean byte is not zero or one (got {byte})");
+        Ok(byte == 1)
+    }
+
     fn read_u8(&mut self) -> anyhow::Result<u8> {
-        let &[byte] = self.read_bytes_const::<1>()?;
+        let &[byte] = self.read_bytes_const()?;
         Ok(byte)
     }
 
     fn read_i8(&mut self) -> anyhow::Result<i8> {
-        Ok(self.read_u8()? as i8)
+        let &[byte] = self.read_bytes_const()?;
+        Ok(byte as i8)
+    }
+
+    fn read_u16(&mut self) -> anyhow::Result<u16> {
+        Ok(u16::from_be_bytes(*self.read_bytes_const()?))
+    }
+
+    fn read_i16(&mut self) -> anyhow::Result<i16> {
+        Ok(i16::from_be_bytes(*self.read_bytes_const()?))
+    }
+
+    fn read_u32(&mut self) -> anyhow::Result<u32> {
+        Ok(u32::from_be_bytes(*self.read_bytes_const()?))
+    }
+
+    fn read_i32(&mut self) -> anyhow::Result<i32> {
+        Ok(i32::from_be_bytes(*self.read_bytes_const()?))
     }
 
     fn read_u64(&mut self) -> anyhow::Result<u64> {
-        Ok(u64::from_be_bytes(*self.read_bytes_const::<8>()?))
+        Ok(u64::from_be_bytes(*self.read_bytes_const()?))
     }
 
     fn read_i64(&mut self) -> anyhow::Result<i64> {
-        Ok(self.read_u64()? as i64)
+        Ok(i64::from_be_bytes(*self.read_bytes_const()?))
+    }
+
+    fn read_u128(&mut self) -> anyhow::Result<u128> {
+        Ok(u128::from_be_bytes(*self.read_bytes_const()?))
+    }
+
+    fn read_i128(&mut self) -> anyhow::Result<i128> {
+        Ok(i128::from_be_bytes(*self.read_bytes_const()?))
+    }
+
+    fn read_f32(&mut self) -> anyhow::Result<f32> {
+        Ok(f32::from_be_bytes(*self.read_bytes_const()?))
+    }
+
+    fn read_f64(&mut self) -> anyhow::Result<f64> {
+        Ok(f64::from_be_bytes(*self.read_bytes_const()?))
     }
 }
+
+pub const DEFAULT_MAX_STRING_LENGTH: u32 = 32767;
 
 #[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Hash, Default, Debug)]
 pub struct McWriter<W>(pub W);
@@ -179,6 +358,29 @@ pub struct McWriter<W>(pub W);
 impl<W: Write> McWrite for McWriter<W> {
     fn write_bytes(&mut self, bytes: &[u8]) -> anyhow::Result<()> {
         self.0.write_all(bytes).map_err(|e| e.into())
+    }
+}
+
+impl<W> From<W> for McWriter<W> {
+    fn from(value: W) -> Self {
+        Self(value)
+    }
+}
+
+impl<'a> McRead<'a> for Cursor<&'a [u8]> {
+    fn read_bytes(&mut self, count: usize) -> anyhow::Result<&'a [u8]> {
+        let remaining_slice =
+            &self.get_ref()[self.position().min(self.get_ref().len() as u64) as usize..];
+
+        ensure!(
+            remaining_slice.len() <= count,
+            "attempt to read {count} bytes, but cursor has {} bytes remaining",
+            remaining_slice.len()
+        );
+
+        self.set_position(self.position() + count as u64);
+
+        Ok(&remaining_slice[..count])
     }
 }
 
@@ -193,5 +395,59 @@ impl<'a> McRead<'a> for &'a [u8] {
         let (l, r) = self.split_at(count);
         *self = r;
         Ok(l)
+    }
+}
+
+/// Prevents preallocating too much memory in case we get a malicious or invalid
+/// sequence length.
+fn cautious_capacity<Element>(size_hint: usize) -> usize {
+    const MAX_PREALLOC_BYTES: usize = 1024 * 1024;
+
+    if mem::size_of::<Element>() == 0 {
+        0
+    } else {
+        size_hint.min(MAX_PREALLOC_BYTES / mem::size_of::<Element>())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn write_read_array() {
+        let mut buf: McWriter<Vec<u8>> = McWriter(vec![]);
+
+        buf.write_str("abc").unwrap();
+        buf.write_str("123").unwrap();
+
+        let mut reader = buf.0.as_slice();
+
+        let res: [String; 2] = reader
+            .read_array(|r| r.read_str().map(String::from))
+            .unwrap();
+
+        let _: [&str; 0] = reader.read_array(|r| r.read_str()).unwrap();
+
+        assert_eq!(&res, &["abc", "123"]);
+    }
+
+    #[test]
+    #[should_panic = "expect the unexpected"]
+    fn read_array_panic() {
+        let mut buf: McWriter<Vec<u8>> = McWriter(vec![]);
+
+        buf.write_str("abc").unwrap();
+        buf.write_str("123").unwrap();
+
+        let mut reader = buf.0.as_slice();
+
+        let _: [String; 2] = reader
+            .read_array(|r| {
+                let s = r.read_str()?;
+                assert_eq!(s, "abc", "expect the unexpected");
+                Ok(s.into())
+            })
+            .unwrap();
     }
 }
--- crates/valence_protocol/src/packets.rs
+++ crates/valence_protocol/src/packets.rs
@@ -6,44 +6,4 @@ pub mod query_ping_c2s;
 
 pub use handshake_c2s::HandshakeC2s;
 
-/*
-macro_rules! declare_packets {
-    (
-        $(
-            $state:ty => {
-                $(
-                    $side:ty => {
-                        $(
-                            ($id:expr, $packet:ty)
-                        )*
-                    }
-                )*
-            }
-        )*
-    ) => {
-        $(
-            $(
-                $(
-                    impl crate::PacketMeta<$state, $side> for $packet {
-                        const ID: i32 = $id;
-                    }
-                )*
-            )*
-        )*
-    }
-}
 
-// macro_rules! decl_body {
-//     ($id:expr)
-// }
-
-use crate::{Clientbound, Configuration, Handshaking, Login, Serverbound, Status, id};
-
-declare_packets! {
-    Handshaking => {
-        Clientbound => {
-            (id::handshaking::HANDSHAKE_C2S, HandshakeC2s<'_>)
-        }
-    }
-}
-*/
