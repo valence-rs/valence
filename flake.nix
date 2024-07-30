3c11ad0b11b3e35fbb0133ae00b881d01a01ed56
TsubakiDev <i@tsubakidev.cc>
2024-07-30 14:24:34 +0800

    nix: add aarch64 support

--- flake.nix
+++ flake.nix
@@ -11,7 +11,7 @@
 
   outputs = { self, nixpkgs, flake-utils, rust-overlay, ... }:
   flake-utils.lib.eachSystem
-    [ "x86_64-linux" ]
+    [ "x86_64-linux" "aarch64-linux" ]
     (system:
     let
       overlays = [ (import rust-overlay) ];
