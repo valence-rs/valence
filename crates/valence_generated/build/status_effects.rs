a364f4e591308bf99fcd2aa320c9b7d4f49b83e6
jack crump-leys <jackcrumpleys@gmail.com>
2024-08-25 08:11:29 +1200

    remove non-extractor changes

--- crates/valence_generated/build/status_effects.rs
+++ crates/valence_generated/build/status_effects.rs
@@ -15,7 +15,7 @@ pub(crate) enum StatusEffectCategory {
 pub(crate) struct AttributeModifiers {
     attribute: u8,
     operation: u8,
-    base_value: f64,
+    value: f64,
     uuid: String,
 }
 
@@ -143,14 +143,14 @@ pub(crate) fn build() -> anyhow::Result<TokenStream> {
                 let modifiers = modifiers.iter().map(|modifier| {
                     let attribute = &modifier.attribute;
                     let operation = &modifier.operation;
-                    let base_value = &modifier.base_value;
+                    let value = &modifier.value;
                     let uuid = &modifier.uuid;
 
                     quote! {
                         AttributeModifier {
                             attribute: EntityAttribute::from_id(#attribute).unwrap(),
                             operation: EntityAttributeOperation::from_raw(#operation).unwrap(),
-                            base_value: #base_value,
+                            value: #value,
                             uuid: Uuid::parse_str(#uuid).unwrap(),
                         }
                     }
@@ -180,8 +180,8 @@ pub(crate) fn build() -> anyhow::Result<TokenStream> {
             pub attribute: EntityAttribute,
             /// The operation that this modifier applies.
             pub operation: EntityAttributeOperation,
-            /// The base value of this modifier.
-            pub base_value: f64,
+            /// The value of this modifier.
+            pub value: f64,
             /// The UUID of this modifier.
             pub uuid: Uuid,
         }
