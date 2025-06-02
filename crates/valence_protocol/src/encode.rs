f1cf42ad52e8dd437178b211a822a6f4e38db50d
cubodix <cubodix@gmail.com>
2025-06-01 22:07:49 -0300

    enframe_from, prepend_frame, append_frame, methods for PacketEncoder

--- crates/valence_protocol/src/encode.rs
+++ crates/valence_protocol/src/encode.rs
@@ -37,6 +37,110 @@ impl PacketEncoder {
         self.buf.extend_from_slice(bytes)
     }
 
+    /// frames the bytes in a range from `from` to the end of the buffer, framing is:
+    ///     adding a packet length varint to the start of the frame
+    ///     adding a data length varint after the packet length, if compression is enabled
+    ///     compressing the packet, if compression is enabled
+    fn enframe_from(&mut self, from: usize) -> anyhow::Result<()> {
+		let data_len = self.buf.len() - from;
+
+		#[cfg(feature = "compression")]
+		if self.threshold.0 >= 0 {
+			use std::io::Read;
+
+			use flate2::bufread::ZlibEncoder;
+			use flate2::Compression;
+
+			if data_len > self.threshold.0 as usize {
+				let mut z = ZlibEncoder::new(&self.buf[from..], Compression::new(4));
+
+				self.compress_buf.clear();
+
+				let data_len_size = VarInt(data_len as i32).written_size();
+
+				let packet_len = data_len_size + z.read_to_end(&mut self.compress_buf)?;
+
+				ensure!(
+					packet_len <= MAX_PACKET_SIZE as usize,
+					"packet exceeds maximum length"
+				);
+
+				drop(z);
+
+				self.buf.truncate(from);
+
+				let mut writer = (&mut self.buf).writer();
+
+				VarInt(packet_len as i32).encode(&mut writer)?;
+				VarInt(data_len as i32).encode(&mut writer)?;
+				self.buf.extend_from_slice(&self.compress_buf);
+			} else {
+				let data_len_size = 1;
+				let packet_len = data_len_size + data_len;
+
+				ensure!(
+					packet_len <= MAX_PACKET_SIZE as usize,
+					"packet exceeds maximum length"
+				);
+
+				let packet_len_size = VarInt(packet_len as i32).written_size();
+
+				let data_prefix_len = packet_len_size + data_len_size;
+
+				self.buf.put_bytes(0, data_prefix_len);
+				self.buf
+					.copy_within(from..from + data_len, from + data_prefix_len);
+
+				let mut front = &mut self.buf[from..];
+
+				VarInt(packet_len as i32).encode(&mut front)?;
+				// Zero for no compression on this packet.
+				VarInt(0).encode(front)?;
+			}
+
+			return Ok(());
+		}
+
+		let packet_len = data_len;
+
+		ensure!(
+			packet_len <= MAX_PACKET_SIZE as usize,
+			"packet exceeds maximum length"
+		);
+
+		let packet_len_size = VarInt(packet_len as i32).written_size();
+
+		self.buf.put_bytes(0, packet_len_size);
+		self.buf
+			.copy_within(from..from + data_len, from + packet_len_size);
+
+		let front = &mut self.buf[from..];
+		VarInt(packet_len as i32).encode(front)?;
+
+		Ok(())
+	}
+
+    pub fn prepend_frame<P>(&mut self, frame: &[u8]) -> anyhow::Result<()>
+    where
+        P: Packet + Encode,
+    {
+        let start_len = self.buf.len();
+        self.append_frame(frame)?;
+
+        let end_len = self.buf.len();
+        let total_packet_len = end_len - start_len;
+
+        // 1) Move everything back by the length of the packet.
+        // 2) Move the packet to the new space at the front.
+        // 3) Truncate the old packet away.
+        self.buf.put_bytes(0, total_packet_len);
+        self.buf.copy_within(..end_len, total_packet_len);
+        self.buf.copy_within(total_packet_len + start_len.., 0);
+        self.buf.truncate(end_len);
+
+        Ok(())
+    }
+
     pub fn prepend_packet<P>(&mut self, pkt: &P) -> anyhow::Result<()>
     where
         P: Packet + Encode,
@@ -57,93 +161,24 @@ impl PacketEncoder {
 
         Ok(())
     }
-
-    #[allow(clippy::needless_borrows_for_generic_args)]
-    pub fn append_packet<P>(&mut self, pkt: &P) -> anyhow::Result<()>
-    where
-        P: Packet + Encode,
-    {
-        let start_len = self.buf.len();
-
-        pkt.encode_with_id((&mut self.buf).writer())?;
-
-        let data_len = self.buf.len() - start_len;
-
-        #[cfg(feature = "compression")]
-        if self.threshold.0 >= 0 {
-            use std::io::Read;
-
-            use flate2::bufread::ZlibEncoder;
-            use flate2::Compression;
-
-            if data_len > self.threshold.0 as usize {
-                let mut z = ZlibEncoder::new(&self.buf[start_len..], Compression::new(4));
-
-                self.compress_buf.clear();
-
-                let data_len_size = VarInt(data_len as i32).written_size();
-
-                let packet_len = data_len_size + z.read_to_end(&mut self.compress_buf)?;
-
-                ensure!(
-                    packet_len <= MAX_PACKET_SIZE as usize,
-                    "packet exceeds maximum length"
-                );
-
-                drop(z);
-
-                self.buf.truncate(start_len);
-
-                let mut writer = (&mut self.buf).writer();
-
-                VarInt(packet_len as i32).encode(&mut writer)?;
-                VarInt(data_len as i32).encode(&mut writer)?;
-                self.buf.extend_from_slice(&self.compress_buf);
-            } else {
-                let data_len_size = 1;
-                let packet_len = data_len_size + data_len;
-
-                ensure!(
-                    packet_len <= MAX_PACKET_SIZE as usize,
-                    "packet exceeds maximum length"
-                );
-
-                let packet_len_size = VarInt(packet_len as i32).written_size();
-
-                let data_prefix_len = packet_len_size + data_len_size;
-
-                self.buf.put_bytes(0, data_prefix_len);
-                self.buf
-                    .copy_within(start_len..start_len + data_len, start_len + data_prefix_len);
-
-                let mut front = &mut self.buf[start_len..];
-
-                VarInt(packet_len as i32).encode(&mut front)?;
-                // Zero for no compression on this packet.
-                VarInt(0).encode(front)?;
-            }
-
-            return Ok(());
-        }
-
-        let packet_len = data_len;
-
-        ensure!(
-            packet_len <= MAX_PACKET_SIZE as usize,
-            "packet exceeds maximum length"
-        );
-
-        let packet_len_size = VarInt(packet_len as i32).written_size();
-
-        self.buf.put_bytes(0, packet_len_size);
-        self.buf
-            .copy_within(start_len..start_len + data_len, start_len + packet_len_size);
-
-        let front = &mut self.buf[start_len..];
-        VarInt(packet_len as i32).encode(front)?;
-
-        Ok(())
-    }
+	
+	pub fn append_frame(&mut self, frame: &[u8]) -> anyhow::Result<()> {
+	    let start_len = self.buf.len();
+        self.append_bytes(frame);
+	    self.enframe_from(start_len)?;
+	    Ok(())
+	}
+	
+	#[allow(clippy::needless_borrows_for_generic_args)]
+	pub fn append_packet<P>(&mut self, pkt: &P) -> anyhow::Result<()>
+	where
+		P: Packet + Encode,
+	{
+		let start_len = self.buf.len();
+		pkt.encode_with_id((&mut self.buf).writer())?;
+		self.enframe_from(start_len)?;
+		Ok(())
+	}
 
     /// Takes all the packets written so far and encrypts them if encryption is
     /// enabled.
